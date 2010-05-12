2a26a8626dc2d6043b4c41d45c830daa06aa2060
Raven <RavensKrag@gmail.com>
2010-05-11 20:54:48 -0400

    Create rudimentary 3D Body as CP::Body_3D
    Holds two CP::Shape objects, each with an attached CP::Body.
    Provides abstraction from both CP::Shape and CP::Body for applications
    in the psudo-3D space constucted by CP::Space_3D.  As a result, the
    CP::Space and CP::Body objects should not be directly accessable.

--- ChipmunkInterfaceMod.rb
+++ ChipmunkInterfaceMod.rb
@@ -7,6 +7,12 @@ require 'chipmunk'
 require 'gosu'
 require 'RMagick'
 
+#NOTES
+#Implement a 3D body
+#	This allows for calls to look like this
+#		body = CP::Body_3D
+#		body.x
+
 module CP
 	class Space
 		def add(shape)
@@ -51,6 +57,43 @@ module CP
 		def add_static_shape=(arg)
 			@xy.add_static_shape = arg
 			@xz.add_static_shape = arg
+		end
+	end
+	
+	class Body_3D
+		def initialize
+			#Bounding box sizes are place holders.  Change them later.
+			@xy = CP::Shape::Poly.new(CP::Body.new(10, 150), 
+										CP::Shape::Polygon.vertices(4, 10), #10 sq units square
+										CP::Vec2.new(x, y))
+			@xz = CP::Shape::Poly.new(CP::Body.new(10, 150), 
+										CP::Shape::Polygon.vertices(4, 10),	#10 sq units square
+										CP::Vec2.new(x, z))
+		end
+		
+		def x
+			@xy.body.p.x
+		end
+		
+		def y
+			@xy.body.p.y
+		end
+		
+		def z
+			@xz.body.p.y
+		end
+		
+		def x= arg
+			@xy.body.p.x = arg
+			@xz.body.p.x = arg
+		end
+		
+		def y= arg
+			@xy.body.p.y = arg
+		end
+		
+		def z= arg
+			@xz.body.p.y = arg
 		end
 	end
 	
