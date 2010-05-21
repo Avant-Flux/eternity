e9fecae9c1c9a5bce38cbc098b2188874b373968
Raven <RavensKrag@gmail.com>
2010-05-21 13:27:23 -0400

    Create placeholder collision functions for Building_Body.

--- CPBody_3D.rb
+++ CPBody_3D.rb
@@ -192,6 +192,14 @@ module CP
 		def initialize(space, x, y, z, width, height)
 			super(space, x, y, z, width, height, :entity_bottom, :entity_side, 
 					Float::INFINITY, Float::INFINITY)
+			
+			xy_collision_fx do |b1_shape, b2_shape|
+				1
+			end
+			
+			xz_collision_fx do |s1_shape, s2_shape|
+				nil
+			end
 		end
 	end
 end
