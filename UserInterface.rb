1e0fc0c525b02c93a035c8a7f86b0af1b9ed14cc
Raven <RavensKrag@gmail.com>
2010-06-01 14:58:17 -0400

    Add methods #update and #draw to mimic the style of the Gosu::Window.

--- UserInterface.rb
+++ UserInterface.rb
@@ -6,10 +6,28 @@ class Hud
 	def initialize
 		
 	end
+	
+	def update
+		
+	end
+	
+	def draw
+		
+	end
 end
 
+#This class renders the overlay which shows the position of tracked entities
+#	
 class Tracking_Overlay
 	def initialize
+		
+	end
+	
+	def update
+		
+	end
+	
+	def draw
 		
 	end
 end
