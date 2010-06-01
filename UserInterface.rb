bd5e40a91c7ac99081e1b2a91fb0f8a6320f186a
Raven <RavensKrag@gmail.com>
2010-06-01 15:05:40 -0400

    Add comment describing general algorithm for creating the tracking overlay.

--- UserInterface.rb
+++ UserInterface.rb
@@ -17,7 +17,12 @@ class Hud
 end
 
 #This class renders the overlay which shows the position of tracked entities
-#	
+#	In order to render this effect is psudo-perspective without a global perspective warp,
+#	render the tracking blips along an elipse which simulates a circle in perpective.
+#
+#	Calculate an elipse and project the rendering of the blip onto it.
+#	Calculate the position of the blip by getting the angle between the player and the entity
+#	to be tracked.
 class Tracking_Overlay
 	def initialize
 		
