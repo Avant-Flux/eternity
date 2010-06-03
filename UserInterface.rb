f9c3c717a4e5b25b4e09875f2ae84bab30624c95
Raven <RavensKrag@gmail.com>
2010-06-03 03:17:39 -0400

    Center the ellipse on the player.
    The ellipse is centered on the player, but the player is being drawn
    with the top right of the sprite at the stored point, not the feet
    of the character.

--- UserInterface.rb
+++ UserInterface.rb
@@ -71,7 +71,7 @@ class Tracking_Overlay
 	
 	def draw
 		Blip.all.each {|b| b.draw}
-		@ellipse.draw 12, 12, 12+10
+		@ellipse.draw @player.body.x-120, @player.body.y-80, @player.body.z+10
 	end
 	
 	private
