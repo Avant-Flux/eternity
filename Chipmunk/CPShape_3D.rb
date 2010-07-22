859a44c153679dab8e1dab77eb46d816d1ebee54
Raven <RavensKrag@gmail.com>
2010-07-21 22:48:10 -0400

    Move Eternity specific Chipmunk-related code to CPEntity.rb

--- Chipmunk/CPShape_3D.rb
+++ Chipmunk/CPShape_3D.rb
@@ -8,15 +8,26 @@ require 'rubygems'
 require 'chipmunk'
 
 #Allows collision functions to be defined from within the shape
-module Collide
-	def collision_fx(type1, type2, &block)
-		@space.add_collision_func(type1, type2) do |shape1, shape2|
-			yield shape1, shape2
+module CP
+	module Collide
+		def collision_fx(type1, type2=self, &block)
+			@space.add_collision_func(type1, type2) do |shape1, shape2|
+				yield shape1, shape2
+			end
 		end
 	end
-end
+	
+	module Position #Have setters and getters for position and velocity
+		def x
+		end
+		
+		def y
+		end
+		
+		def z
+		end
+	end
 
-module CP
 	module Shape
 		class Rect < CP::Shape::Poly
 			def initialize(body, center=:bottom, width, height, offset)
@@ -91,7 +102,8 @@ module CP
 
 	module Shape_3D
 		class Circle < CP::Shape::Circle
-			include Collide
+			include CP::Collide
+			include CP::Position
 			
 			def initialize(space, collision, mass, moment, radius, offset)
 				@space = space
@@ -105,6 +117,9 @@ module CP
 		end
 		
 		class Rect < CP::Shape::Rect
+			include CP::Collide
+			include CP::Position
+		
 			def initialize(space, center, width, height, mass, moment, offset)
 				@space = space
 				collision_type = collision
@@ -119,72 +134,6 @@ module CP
 	
 
 	#~ class Shape_3D
-		#~ attr_reader :xy, :xz, :space
-	#~ 
-		#~ def initialize(space, x, y, z, width, height, 
-		#~ xy_collision_type, xz_collision_type, mass, moment)
-			#~ @width = width
-			#~ @height = height
-			#~ 
-			#~ @space = space
-			#~ 
-			#~ half_width = width/2
-			#~ #Bottom Shape
-			#~ @xy = CP::Shape::Circle.new(CP::Body.new(mass, moment), half_width, CP::Vec2.new(0, 0))
-			#~ @xy.body.p = CP::Vec2.new(x, y)
-										#~ 
-			#~ @xy.body.a = (3*Math::PI/2.0)
-			#~ 
-			#~ @xy.collision_type = xy_collision_type
-		#~ end
-		#~ 
-		#~ def transfer_x
-			#~ #Transfer the x component of force on xz to xy
-			#~ #Then, copy the x component of position from xy to xz
-			#~ apply_force :xy, CP::Vec2.new(f(:xz).x,0)
-			#~ f(:xz).x = 0
-			#~ @xz.body.p.x = @xy.body.p.x
-		#~ end
-		#~ 
-		#~ def apply_gravity
-			#~ if self.z < 0
-				#~ self.z = 0
-			#~ else
-				#~ gravity = @xz.body.m * 9.8 * -1
-				#~ apply_force(:xz, CP::Vec2.new(0, gravity), CP::Vec2.new(0,0))
-			#~ end
-		#~ end
-		#~ 
-		#~ def apply_force(plane, force_vector, relative_offset=CP::Vec2.new(0,0))
-			#~ if plane == :xy
-				#~ @xy.body.apply_force(force_vector, relative_offset)
-			#~ elsif plane == :xz
-				#~ @xz.body.apply_force(force_vector, relative_offset)
-			#~ end
-		#~ end
-		#~ 
-		#~ def reset_forces(plane)
-			#~ if plane == :xy
-				#~ @xy.body.reset_forces
-			#~ elsif plane == :xz
-				#~ @xz.body.reset_forces
-			#~ elsif plane == :all
-				#~ @xy.body.reset_forces
-				#~ @xz.body.reset_forces
-			#~ end
-		#~ end
-		#~ 
-		#~ #Setters and getters for force
-		#~ def f plane
-			#~ p = if plane == :xy
-				#~ @xy
-			#~ elsif plane == :xz
-				#~ @xz
-			#~ end
-			#~ 
-			#~ p.body.f
-		#~ end
-		#~ 
 		#~ #Setters and getters should be changed to use aliases
 		#~ #Setters and Getters for Position
 		#~ def x
@@ -268,55 +217,6 @@ module CP
 		#~ def a= arg
 			#~ @xy.body.a = arg
 		#~ end
-		#~ 
-		#~ private
-		#~ 
-		#~ def xy_collision_fx(&block)
-			#~ @space.xy.add_collision_func(@xy.collision_type, @xy.collision_type) do |b1_shape, b2_shape|
-				#~ yield b1_shape, b2_shape
-			#~ end
-		#~ end
-		#~ 
-		#~ def xz_collision_fx(&block)
-			#~ @space.xz.add_collision_func(@xz.collision_type, @xz.collision_type) do |s1_shape, s2_shape|
-				#~ yield s1_shape, s2_shape
-			#~ end
-		#~ end
-	#~ end
-	
-	#~ class Entity_Body < Body_3D
-		#~ def initialize(space, x, y, z, width, height, mass=100, moment=150)
-			#~ super(space, x, y, z, width, height, :entity_bottom, :entity_side, mass, moment)
-			#~ 
-			#~ xy_collision_fx do |b1_shape, b2_shape|
-				#~ if 
-					#~ nil
-				#~ else
-					#~ nil
-				#~ end
-				#~ 1
-			#~ end
-			#~ 
-			#~ xz_collision_fx do |s1_shape, s2_shape|
-				#~ nil
-			#~ end
-		#~ end
-	#~ end
-	#~ 
-	#~ class Building_Body < Body_3D
-		#~ def initialize(space, x, y, z, width, height)
-			#~ super(space, x, y, z, width, height, :building_bottom, :building_side, 
-					#~ Float::INFINITY, Float::INFINITY)
-			#~ 
-			#~ xy_collision_fx do |b1_shape, b2_shape|
-				#~ 1
-			#~ end
-			#~ 
-			#~ xz_collision_fx do |s1_shape, s2_shape|
-				#~ nil
-			#~ end
-		#~ end
-	#~ end
 end
 
 
