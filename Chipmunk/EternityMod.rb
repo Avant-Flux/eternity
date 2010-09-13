10615ab1bb4cc5d4aeda4717b3bedac47d547f82
Raven <RavensKrag@gmail.com>
2010-09-13 17:05:13 -0400

    Remove unnecessary code.

--- Chipmunk/EternityMod.rb
+++ Chipmunk/EternityMod.rb
@@ -54,42 +54,6 @@ module CollisionHandler
 	end
 end
 
-#This file should contain all Eternity-specific Chipmunk-related code 
-
-#~ class Entity_Body < Body_3D
-	#~ def initialize(space, x, y, z, width, height, mass=100, moment=150)
-		#~ super(space, x, y, z, width, height, :entity_bottom, :entity_side, mass, moment)
-		#~ 
-		#~ xy_collision_fx do |b1_shape, b2_shape|
-			#~ if 
-				#~ nil
-			#~ else
-				#~ nil
-			#~ end
-			#~ 1
-		#~ end
-		#~ 
-		#~ xz_collision_fx do |s1_shape, s2_shape|
-			#~ nil
-		#~ end
-	#~ end
-#~ end
-#~ 
-#~ class Building_Body < Body_3D
-	#~ def initialize(space, x, y, z, width, height)
-		#~ super(space, x, y, z, width, height, :building_bottom, :building_side, 
-				#~ Float::INFINITY, Float::INFINITY)
-		#~ 
-		#~ xy_collision_fx do |b1_shape, b2_shape|
-			#~ 1
-		#~ end
-		#~ 
-		#~ xz_collision_fx do |s1_shape, s2_shape|
-			#~ nil
-		#~ end
-	#~ end
-#~ end
-
 module CP
 	class Space_3D
 		@@scale = 44
