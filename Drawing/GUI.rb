8e9fcc4f0ce21568383d85982b02f87d0fabfd98
Raven <RavensKrag@gmail.com>
2010-09-17 14:23:44 -0400

    Create new file to hold GUI elements.

---
+++ Drawing/GUI.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+#~ Name: Jason
+#~ Date last edited: 09.16.2010
+ 
+module GUI
+	class Tooltip
+		#~ Defines the tooltips that appear on mouseover
+		def initialize
+			
+		end
+	end
+end
