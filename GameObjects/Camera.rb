60bd91fb941b1158d1e9afd3130db801b5bd12d9
Raven <RavensKrag@gmail.com>
2010-09-18 22:12:15 -0400

    Create skeletal implementation of Camera.

--- GameObjects/Camera.rb
+++ GameObjects/Camera.rb
@@ -2,9 +2,19 @@
 #~ Name: Jason
 #~ Date last edited: 09.18.2010
 require 'set'
+
+require 'rubygems'
+require 'gosu'
+require 'chipmunk'
+require 'Chipmunk/Shape'
+require 'Chipmunk/EternityMod'
  
 class Camera
 	def initialize(width, depth, entity)
+		mass = entity.shape.body.m
+		@shape = CP::Shape::Rect.new(CP::Body.new(mass, Float::INFINITY), :top_left, width, depth)
+		@entity = entity
+		
 		@queue = Set.new
 	end
 end
