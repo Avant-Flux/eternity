c8a04fb9d423460756560ccafadf9518b304cf1f
Raven <RavensKrag@gmail.com>
2010-09-25 15:18:02 -0400

    Update OSX installer.

--- install_osx.sh
+++ install_osx.sh
@@ -1,9 +1,3 @@
-sudo apt-get install ruby1.9.1-dev
-sudo apt-get install libgl1-mesa-dri libglu1-mesa freeglut3 libgl1-mesa-dev libglu1-mesa-dev freeglut3-dev
-sudo apt-get install libopengl-ruby1.9
-sudo apt-get install g++ libgl1-mesa-dev libpango1.0-dev libboost-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev
-sudo apt-get install libmagickcore-dev libmagickwand-dev
-
 sudo gem install rake
 sudo gem install ruby-opengl
 sudo gem install gosu
