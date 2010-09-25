5c18d2e7a0bc828b2f5ffa172728c9a7a38ed1b5
Raven <RavensKrag@gmail.com>
2010-09-25 15:17:44 -0400

    Create new installer files in bash for osx and ubuntu.

---
+++ install_ubuntu.sh
@@ -0,0 +1,13 @@
+sudo apt-get install ruby1.9.1-dev
+sudo apt-get install libgl1-mesa-dri libglu1-mesa freeglut3 libgl1-mesa-dev libglu1-mesa-dev freeglut3-dev
+sudo apt-get install libopengl-ruby1.9
+sudo apt-get install g++ libgl1-mesa-dev libpango1.0-dev libboost-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev
+sudo apt-get install libmagickcore-dev libmagickwand-dev
+
+sudo gem install rake
+sudo gem install ruby-opengl
+sudo gem install gosu
+sudo gem install chingu
+sudo gem install chipmunk
+sudo gem install texplay
+sudo gem install eventmachine
