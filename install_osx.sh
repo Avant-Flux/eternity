6e8cd41dcacc5ba7cbc6b1c666612fd2a7600b8a
Raven <RavensKrag@gmail.com>
2010-10-02 20:22:27 -0400

    Condense the OSX installer into a single line.
    This however, assumes that the user already has a working ruby installation.

--- install_osx.sh
+++ install_osx.sh
@@ -1,7 +1,3 @@
 #~ sudo gem install rake #rake is installed by default
-sudo gem install ruby-opengl #no dependencies
-sudo gem install gosu
-sudo gem install chingu
-sudo gem install chipmunk
-sudo gem install texplay
-sudo gem install eventmachine
+#Gems do not appear to need separate dependencies on OSX
+sudo gem install ruby-opengl gosu chingu chipmunk texplay eventmachine
