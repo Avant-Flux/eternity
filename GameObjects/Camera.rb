6201c1379228314e9dce4b26d91d1dcd04426ae6
Raven <RavensKrag@gmail.com>
2010-10-10 15:45:43 -0400

    Get the camera to correctly sense objects in the CP::Space_3D.
    Camera had to have a circle as the sensor.  There thus seems to be something wrong
    with the implementation of CP::Shape::Rect, at least when it is generated from the
    upper-left corner.

--- GameObjects/Camera.rb
+++ GameObjects/Camera.rb
@@ -13,33 +13,34 @@ class Camera
 	attr_reader :shape, :queue
 
 	def initialize(space, entity)
-		@entity = entity
 		@space = space
+		@entity = entity
+		
 		@width = $window.width.to_meters
 		@depth = $window.height.to_meters
 		
 		mass = @entity.shape.body.m
-		@shape = CP::Shape::Rect.new(CP::Body.new(mass, Float::INFINITY), :top_left, @width, @depth)
+		#~ @shape = CP::Shape_3D::Rect.new(self, space, :camera, [@entity.shape.x-@width/2.0, @entity.shape.y-@depth/2.0, 0], 0, :top_left, 20, 20, 1, mass, Float::INFINITY)
+		#~ @shape = CP::Shape::Rect.new(CP::Body.new(mass, Float::INFINITY), :top_left, @width, @depth)
+		@shape = CP::Shape::Circle.new(CP::Body.new(mass, Float::INFINITY), @depth, CP::Vec2.new(0,0))
 		
+		@shape.sensor = true
 		@shape.collision_type = :camera
 		@shape.body.a = (3*Math::PI/2.0)
 		@shape.body.p = CP::Vec2.new(@entity.shape.x-@width/2.0, @entity.shape.y-@depth/2.0)
-		
-		#~ @shape = CP::Shape_3D::Rect.new(self, space, :camera, [@entity.shape.x-width/2.0, @entity.shape.y-depth/2.0, 0], 0, :top_left, width, depth, 1, mass, Float::INFINITY)
-		
-		@shape.sensor = true
 		
 		space.add_shape @shape
 		space.add_body @shape.body
 		#~ space.add self
 		#~ shapes = space.shapes[:nonstatic].delete(@shape)
 		
-		@queue = Array.new
+		@queue = Set.new
 	end
 	
 	def update
 		@shape.body.reset_forces
 		#~ @queue = @space.active_shapes_hash.query_by_bb BB.new(0,0,@width,@height)
+		p @queue
 	end
 	
 	def move(force, offset=CP::Vec2.new(0,0))
@@ -59,28 +60,22 @@ module CollisionHandler
 	#~ Collision type for usage with the CP::Shape and CP::Body used for the camera
 	#~ This is the collision handler for a sensor object
 	class Camera
-		def initialize(camera) #Argument should be the actual camera
-			@camera = camera
-		end
-		
 		def begin(a, b, arbiter)
-			puts "hey"
-			#~ return true
+			$camera.queue.add b
 		end
 		
-		def pre_solve(a, b, arbiter) #Determine whether to process collision or not
-			puts "on"
+		#~ def pre_solve(a, b, arbiter) #Determine whether to process collision or not
+			#~ puts "on"
 			#~ @camera.queue.add b
-		end
-		
-		def post_solve(a, b, arbiter)
-			puts "yo"
+		#~ end
+		#~ 
+		#~ def post_solve(a, b, arbiter)
+			#~ puts "yo"
 			#~ @camera.queue.add b
-		end
+		#~ end
 		
 		def separate(a, b, arbiter)	#Stuff to do after the shapes separate
-			puts "off"
-			#~ @camera.queue.delete b
+			$camera.queue.delete b
 		end
 	end
 end
