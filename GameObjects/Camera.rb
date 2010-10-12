cb0b5741bd3d316bb8c96650eb6f3360315f3182
Raven <RavensKrag@gmail.com>
2010-10-12 00:45:06 -0400

    Polish code.

--- GameObjects/Camera.rb
+++ GameObjects/Camera.rb
@@ -16,16 +16,16 @@ class Camera
 		@space = space
 		@entity = entity
 		
-		@width = $window.width.to_meters
-		@depth = $window.height.to_meters
+		width = $window.width.to_meters
+		depth = $window.height.to_meters
 		
 		mass = @entity.shape.body.m
 		
-		@shape = CP::Shape::Rect.new(CP::Body.new(mass, Float::INFINITY), :bottom_left, @depth, @width)
+		@shape = CP::Shape::Rect.new(CP::Body.new(mass, Float::INFINITY), :bottom_left, depth, width)
 		
 		@shape.sensor = true
 		@shape.collision_type = :camera
-		@shape.body.p = CP::Vec2.new(@entity.shape.x-@width/2.0, @entity.shape.y-@depth/2.0)
+		@shape.body.p = CP::Vec2.new(@entity.shape.x-width/2.0, @entity.shape.y-depth/2.0)
 		
 		space.add self
 		shapes = space.shapes[:nonstatic].delete(@shape)
@@ -37,7 +37,7 @@ class Camera
 		@shape.body.reset_forces
 	end
 	
-	def move(force, offset=CP::Vec2.new(0,0))
+	def move(force, offset=CP::ZERO_VEC_2)
 		@shape.body.apply_force force, offset
 	end
 	
@@ -54,11 +54,13 @@ module CollisionHandler
 	#~ Collision type for usage with the CP::Shape and CP::Body used for the camera
 	#~ This is the collision handler for a sensor object
 	class Camera
+		#~ a has collision type :camera
+		#~ b is the Chipmunk object for the Entity
 		def begin(a, b, arbiter)
 			$camera.queue.add b.entity
 		end
 				
-		def separate(a, b, arbiter)	#Stuff to do after the shapes separate
+		def separate(a, b, arbiter)
 			$camera.queue.delete b.entity
 		end
 	end
