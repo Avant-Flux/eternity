a8c0581a9f40c77ca92e2cb8ebf8be2e52a67f7b
Raven <RavensKrag@gmail.com>
2010-10-28 02:51:09 -0400

    Refactor code.

--- Chipmunk/Space_3D.rb
+++ Chipmunk/Space_3D.rb
@@ -94,7 +94,7 @@ end
 
 class Numeric
 	#Code taken from MoreChipmunkAndRMagick.rb from the gosu demos
-   def radians_to_vec2
-       CP::Vec2.new(Math::cos(self), Math::sin(self))
-   end
+	def radians_to_vec2
+		CP::Vec2.new(Math::cos(self), Math::sin(self))
+	end
 end
