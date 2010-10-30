444c5243703523a0852a2d9f90e46a2e104da105
Raven <RavensKrag@gmail.com>
2010-10-30 04:40:05 -0400

    Revert Wireframe::Building to use drawing methods without the block call.
    The block method suddenly stopped working for some reason.  This could be
    an indication of a problem elsewhere in the code.

--- Drawing/Wireframe.rb
+++ Drawing/Wireframe.rb
@@ -76,36 +76,36 @@ module Wireframe
 			
 			@img = TexPlay.create_blank_image $window,	width + @side_thickness*2 + side_buffer*2, 
 														height + depth + bottom_buffer
-			@img.paint do
+			#~ @img.paint do
 				#Top side, left edge
-				triangle	points[0].x, points[0].y, 
+				@img.triangle	points[0].x, points[0].y, 
 							points[2].x, points[2].y, 
 							points[3].x, points[3].y, :color => color, :fill => true
 				#Top side, right edge
-				triangle	points[1].x, points[1].y, 
+				@img.triangle	points[1].x, points[1].y, 
 							points[4].x, points[4].y, 
 							points[5].x, points[5].y, :color => color, :fill => true
 				#Front side, left edge
-				triangle	points[2].x, points[2].y, 
+				@img.triangle	points[2].x, points[2].y, 
 							points[3].x, points[3].y, 
 							points[8].x, points[8].y, :color => color, :fill => true
 				#Front side, left edge
-				triangle	points[4].x, points[4].y, 
+				@img.triangle	points[4].x, points[4].y, 
 							points[5].x, points[5].y, 
 							points[9].x, points[9].y, :color => color, :fill => true
 				#Top side, back edge
-				line 	points[0].x, points[0].y,
+				@img.line 	points[0].x, points[0].y,
 						points[1].x, points[1].y, :color => color, :thickness => back_edge
 				#Top side, front edge
-				line	points[2].x, points[2].y,
+				@img.line	points[2].x, points[2].y,
 						points[5].x, points[5].y, :color => color, :thickness => front_edge
 				#Concealed edge
-				line	points[6].x, points[6].y,
+				@img.line	points[6].x, points[6].y,
 						points[7].x, points[7].y, :color => color, :thickness => consealed_edge
 				#Front side, bottom edge
-				line	points[8].x, points[8].y,
+				@img.line	points[8].x, points[8].y,
 						points[9].x, points[9].y, :color => color, :thickness => bottom_edge
-			end
+			#~ end
 			
 			@x = @shape.x.to_px - @side_thickness - 10 + side_buffer
 			@y = @shape.y.to_px - @img.height - @shape.z.to_px
