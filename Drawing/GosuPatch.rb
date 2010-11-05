0112e325137fdbb1339912e92315256a2b10c33a
Raven <RavensKrag@gmail.com>
2010-11-04 22:25:00 -0400

    Change the way height and width are called.
    Use the methods with a self call instead of trying to access instance variables.

--- Drawing/GosuPatch.rb
+++ Drawing/GosuPatch.rb
@@ -3,7 +3,7 @@
 module Gosu
 	class Image
 		def draw_centered(x, y, z, factor_x=1, factor_y=1, color=0xffffffff, mode=:default)
-			self.draw(x-@width/2, y-@height/2, z, factor_x, factor_y, color, mode)
+			self.draw(x-self.width/2, y-self.height/2, z, factor_x, factor_y, color, mode)
 		end
 	end
 end
