508512313776ced4a903bb30fdef11a7f209d185
Raven <RavensKrag@gmail.com>
2010-11-12 23:55:50 -0500

    Move code to set elevation into a separate method.
    Increases code readability.

--- Chipmunk/EternityMod.rb
+++ Chipmunk/EternityMod.rb
@@ -60,14 +60,7 @@ module CollisionHandler
 			if a.z < b.height #If the entity collides from the side, accept the collision
 				return true
 			else
-				all_ones = 2**32-1
-				a.space.point_query CP::Vec2.new(a.x,a.y), all_ones,0 do |env|
-					if env.is_a?(CP::Shape_3D::Rect) || env.is_a?(CP::Shape_3D::Circle)
-						if env.height > a.elevation
-							a.elevation = env.height
-						end
-					end
-				end
+				set_elevation(a,b)
 				
 				return false
 			end
@@ -79,6 +72,19 @@ module CollisionHandler
 		
 		def separate(a,b,arbiter)	#Stuff to do after the shapes separate
 			a.elevation = 0
+		end
+		
+		private
+		
+		def set_elevation(a, b)
+			all_ones = 2**32-1
+			a.space.point_query CP::Vec2.new(a.x,a.y), all_ones,0 do |env|
+				if env.is_a?(CP::Shape_3D::Rect) || env.is_a?(CP::Shape_3D::Circle)
+					if env.height > a.elevation
+						a.elevation = env.height
+					end
+				end
+			end
 		end
 	end
 end
