1df36d1a69b940b6e38d56a39d02dc15c5f92d1c
Raven <RavensKrag@gmail.com>
2010-12-05 14:44:57 -0500

    Rename hash in Building to options.

--- GameObjects/Building.rb
+++ GameObjects/Building.rb
@@ -11,15 +11,15 @@ require './Drawing/Wireframe'
 class Building
 	attr_reader :shape
 
-	def initialize(space, hash={})
+	def initialize(space, options={})
 		#~ Set default values for hash values if they are not already set.
-		hash[:dimensions] ||= [1,1,1]
-		hash[:position] ||= [0,0,0]
-		hash[:offset] ||= CP::Vec2.new(0,0)
+		options[:dimensions] ||= [1,1,1]
+		options[:position] ||= [0,0,0]
+		options[:offset] ||= CP::Vec2.new(0,0)
 		
-		@shape = CP::Shape_3D::Rect.new(self, :building, hash[:position], 0, :bottom_left, 
-							hash[:dimensions][0], hash[:dimensions][1], hash[:dimensions][2], 
-							Float::INFINITY, Float::INFINITY, hash[:offset])
+		@shape = CP::Shape_3D::Rect.new(self, :building, options[:position], 0, :bottom_left, 
+							options[:dimensions][0], options[:dimensions][1], options[:dimensions][2], 
+							Float::INFINITY, Float::INFINITY, options[:offset])
 		@wireframe = Wireframe::Building.new(@shape, :white)
 		space.add self
 	end
