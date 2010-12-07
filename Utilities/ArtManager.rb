e117923b4ea7d28336a3f507e47bb709ee1781dd
Raven <RavensKrag@gmail.com>
2010-12-07 16:04:52 -0500

    Rework ArtManager with new top-down design philosophy.
    Only allow access to high-level types.  Manage all the footwork within this
    class.

--- Utilities/ArtManager.rb
+++ Utilities/ArtManager.rb
@@ -4,9 +4,12 @@ class ArtManager
 	def initialize(asset_dir) #Pass the full path to the Sprites/ directory
 		@dir = asset_dir
 		
-		#The structure of the @assets hash should mirror the file structure
-		#Could alternatively use a tree
-		@assets = {:effects => [], :people => [], :textures => []}
+		#Subsprites are raw images
+		#Sprites are fully composited spritesheets which have already been split
+		#Animations are sprites wrapped in a interface layer for easy usage.
+		#	Multiple animations can reference the same sprite
+		@assets = {:subsprite => {}, :sprite => {}, #do not allow direct access of these types
+					:animation => {}, :effects => {}, :textures => {}}
 	end
 	
 	def new_asset(type, name)
@@ -15,9 +18,44 @@ class ArtManager
 		
 		#To make this work, #hash for all objects used should be written
 		#such that the same image will hash to the same location.
+		
+		case type
+			when :animation
+				
+		end
 	end
 	
 	def clear
 		#Empty out all assets
+	end
+	
+	private
+	
+	def load
+		
+	end
+	
+	def load?
+		
+	end
+	
+	def loaded?(type, name)
+		return true if @assets[type][name]
+	end
+	
+	def new_subsprite(name)
+		
+	end
+	
+	def new_sprite
+		
+	end
+	
+	def new_effect
+		
+	end
+	
+	def new_texture
+		
 	end
 end
