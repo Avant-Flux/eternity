7cac93621d8bf61212fc75140f9ee87c4966c23c
Raven <RavensKrag@gmail.com>
2010-12-15 22:55:18 -0500

    Transfer code out of Animation into the new classes.

--- Drawing/Animation.rb
+++ Drawing/Animation.rb
@@ -47,8 +47,6 @@ module Animations
 			
 			#Create a hex number where each two hex-digits corresponds
 			#to one subsprite.
-			
-			
 		end
 		
 		def [](key)
@@ -70,37 +68,6 @@ module Animations
 		
 		private
 		
-		def make_sprites
-			sprite_array = Gosu::Image::load_tiles($window, @spritesheet, 40, 80, false)
-			
-			sprite_array.each_with_index do |sprite, i|
-				#Assumes that the spritesheet is broken up into rows of 8, 
-				#with each column representing the frames to use in one direction
-				key = case i % 8
-					when 0
-						:up_left
-					when 1
-						:left
-					when 2
-						:down_left
-					when 3
-						:down
-					when 4
-						:down_right
-					when 5
-						:right
-					when 6
-						:up_right
-					when 7
-						:up
-				end
-			
-				@sprites[key] << sprite
-			end
-			
-			@current_frame = @sprites[:down][0]
-		end
-		
 		def subsprites basepath, type, subsprite_name
 			return Gosu::Image.new($window, 
 						"#{basepath}/#{type.to_s.capitalize}/#{subsprite_name}.png", false)
@@ -116,17 +83,6 @@ module Animations
 		end
 		
 		private
-		
-		def make_spritesheet body, face, hair, upper, lower, footwear
-			@spritesheet = TexPlay.create_blank_image $window, body.width, body.height
-			
-			@spritesheet.splice(body, 0,0, :alpha_blend => true)
-			@spritesheet.splice(face, 0,0, :alpha_blend => true)
-			@spritesheet.splice(hair, 0,0, :alpha_blend => true)
-			@spritesheet.splice(upper, 0,0, :alpha_blend => true)
-			@spritesheet.splice(lower, 0,0, :alpha_blend => true)
-			@spritesheet.splice(footwear, 0,0, :alpha_blend => true)
-		end
 		
 		def subsprites type, subsprite_name
 			super "./Sprites/People", type, subsprite_name
