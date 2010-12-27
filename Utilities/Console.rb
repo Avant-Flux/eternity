100ae3c5ed26db9e697ae7491b7b80dec1a18b65
Raven <RavensKrag@gmail.com>
2010-12-27 06:21:14 -0500

    Begin implementation of a class for in-engine console output.

---
+++ Utilities/Console.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+
+#This class should reroute standard out and display it in a
+#text box on screen.  This way, errors etc can be displayed
+#in engine.
+class GosuConsole
+	def initialize
+		@output = TextBox.new($window.width, 200)
+	end
+end
