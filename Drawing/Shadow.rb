3d2d9dea6c75475bb35ce536eb0c1f537affa597
Raven <RavensKrag@gmail.com>
2010-12-28 03:41:48 -0500

    Document and clean up code.

--- Drawing/Shadow.rb
+++ Drawing/Shadow.rb
@@ -5,49 +5,57 @@ require 'gosu'
 require 'texplay'
 require './Drawing/GosuPatch'
 
+# Handles the drawing of simple shadows.
 class Shadow
-		def initialize(entity, circle)
-			@entity = entity
-			@circle = circle
-		end
+	OPACITY_CONSTANT = 0.8
+
+	def initialize(entity, circle)
+		@entity = entity
+		@circle = circle
+	end
+	
+	def update
+		@color = color
+		@scale = scale
+	end
+	
+	def draw
+		@circle.draw_centered(@entity.x, @entity.y, @entity.elevation, 
+						:factor_x => @scale, :factor_y => @scale, 
+						:offset_z => -1, :color => @color)
+	end
+	
+	private
+	
+	# Calculate the color of the shadow to be rendered, and mix in
+	# the correct opacity
+	def color
+		color = 0x0000FF		#Current the shadows are blue for testing purposes
+		color += opacity << 24	#Shift the opacity to the correct place value in hex
+	end
+	
+	# Determine the value of the alpha channel.
+	def opacity
+		#Calculate the difference between the terrain and the entity.
+		height = (@entity.z - @entity.elevation)
+		height = 1 if height <= 0
 		
-		def update
-			@color = color
-			@scale = scale
-		end
+		#Compute a percent based on the inverse-square of the distance
+		#and a constant which determines the base opacity.
+		percent = OPACITY_CONSTANT/(height**2)
 		
-		def draw
-			@circle.draw_centered(@entity.x, @entity.y, @entity.elevation, 
-							:factor_x => @scale, :factor_y => @scale, 
-							:offset_z => -1, :color => @color)
-		end
+		#Convert the percent to a two-digit hex value
+		output = (0xFF * percent).to_i
+		#~ puts output.to_s 16
 		
-		private
+		#Limit the output to 0xFF
+		output = 0xFF if output > 0xFF
 		
-		def color
-			#~ Calculate the color of the shadow to be rendered, and mix in
-			#~ The correct opacity
-			color = 0x0000FF
-			color += opacity << 24 #Shift the opacity to the correct place value in hex
-		end
-		
-		def opacity
-			height = (@entity.z - @entity.elevation)
-			height = 1 if height <= 0
-			percent = 1/(height**2)
-			output = (0xFF * percent).to_i	#Convert the percent to a two-digit hex value
-			#~ puts output.to_s 16
-			
-			#Limit the output to 0xFF
-			output = 0xFF if output > 0xFF
-			
-			return output
-		end
-		
-		def scale
-			#~ Calculate the amount by which to scale the shadow
-			
-			#~ 1
-			(@entity.z - @entity.elevation + 1)
-		end
+		return output
+	end
+	
+	# Calculate the amount by which to scale the shadow
+	def scale
+		(@entity.z - @entity.elevation + 1)
+	end
 end
