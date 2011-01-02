383dea4da2331630b55e6441f8f19c012f6c24a9
Raven <RavensKrag@gmail.com>
2011-01-02 03:48:42 -0500

    Change ubuntu installer to install opengl through apt-get not gem.

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ruby_dev="ruby1.9.1-dev"
-ruby_opengl="install libgl1-mesa-dri libglu1-mesa freeglut3 libgl1-mesa-dev libglu1-mesa-dev freeglut3-dev libopengl-ruby1.9.1"
+ruby_opengl="libopengl-ruby1.9.1"
 gosu="g++ libgl1-mesa-dev libpango1.0-dev libboost-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev"
 rmagick="libmagickcore-dev libmagickwand-dev"
 
@@ -9,6 +9,6 @@ install_command="sudo apt-get install"
 
 dependency_installer="$install_command $ruby_dev $ruby_opengl $gosu $rmagick"
 
-eval dependency_installer
+eval $dependency_installer
 
-sudo gem install rake ruby-opengl gosu chingu chipmunk rmagick texplay eventmachine require_all
+sudo gem install rake gosu chingu chipmunk rmagick texplay eventmachine require_all
