ccf7701320ab4f9022462e90e552b9eccddb58ee
Raven <RavensKrag@gmail.com>
2011-01-22 02:36:49 -0500

    Begin to implement framework to draw shadows that split across terrain.

--- Drawing/Shadow.rb
+++ Drawing/Shadow.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/ruby
 
+require 'set'
+
 require 'rubygems'
 require 'gosu'
 require 'texplay'
@@ -12,43 +14,48 @@ class Shadow
 	def initialize(entity, circle)
 		@entity = entity
 		@circle = circle
+		@draw_elevation = Set.new
 	end
 	
 	def update
-		@color = color
-		@scale = scale
+		#~ @color = color
+		#~ @scale = scale
+		
+		
+		#This could calculate scale and color multiple times for the same elevation.
+		@draw_elevation.clear
+		@draw_elevation << 0
+		shapes = $space.active_shapes_hash.query_by_bb CP::BB.new(0,0,0,0) 
+		shapes.each do |env|
+			elevation = env.height
+			scale = scale(env.height)
+			color = color(env.height)
+			
+			@draw_elevation << [elevation, scale, color]
+		end
 	end
 	
 	def draw
-		#~ all_ones = 2**32-1
-		#~ point_query @entity.shape.body.p, all_ones,0 do |env|
-			#~ if env.collision_type == :environment || env.collision_type == :building
-				#~ 
-				#~ 
-				#~ 
-			#~ end
-		#~ end
-		
-		
-		
-		@circle.draw_centered(@entity.x, @entity.y, @entity.elevation, 
-						:factor_x => @scale, :factor_y => @scale, 
-						:offset_z => -1, :color => @color)
+		@draw_elevation.each do |value|
+			@circle.draw_centered(@entity.x, @entity.y, value[0], 
+						:factor_x => value[1], :factor_y => value[1], 
+						:offset_z => -1, :color => value[2])
+		end
 	end
 	
 	private
 	
 	# Calculate the color of the shadow to be rendered, and mix in
 	# the correct opacity
-	def color
-		color = 0x0000FF		#Current the shadows are blue for testing purposes
-		color += opacity << 24	#Shift the opacity to the correct place value in hex
+	def color(ground_height)
+		color = 0x0000FF						#Current the shadows are blue for testing purposes
+		color += opacity(ground_height) << 24	#Shift the opacity to the correct place value in hex
 	end
 	
 	# Determine the value of the alpha channel.
-	def opacity
+	def opacity(ground_height)
 		#Calculate the difference between the terrain and the entity.
-		height = (@entity.z - @entity.elevation)
+		height = (@entity.z - ground_height)
 		height = 1 if height < 1
 		
 		#Compute a percent based on the inverse-square of the distance
@@ -63,7 +70,7 @@ class Shadow
 	end
 	
 	# Calculate the amount by which to scale the shadow
-	def scale
-		(@entity.z - @entity.elevation + 1)
+	def scale(ground_height)
+		(@entity.z - ground_height + 1)
 	end
 end
