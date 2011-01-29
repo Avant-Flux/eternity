63ae09431c3aaf4c9c2fea4be57e24bb225f2759
Raven <RavensKrag@gmail.com>
2011-01-29 13:07:45 -0500

    Update installer to reflect new Gosu dependencies.

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -2,10 +2,10 @@
 
 ruby_dev="ruby1.9.1-dev"
 ruby_opengl="libopengl-ruby1.9.1"
-gosu="g++ libgl1-mesa-dev libpango1.0-dev libboost-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev"
+gosu="g++ libgl1-mesa-dev libpango1.0-dev libboost-dev libaudiere-dev libsdl-ttf2.0-dev"
 rmagick="libmagickcore-dev libmagickwand-dev"
 
-install_command="sudo apt-get install"
+install_command="gksudo apt-get install"
 
 dependency_installer="$install_command $ruby_dev $ruby_opengl $gosu $rmagick"
 
