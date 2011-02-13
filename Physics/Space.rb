8133ff02a911bd093242dceeeae80f5ebd38d632
Raven <RavensKrag@gmail.com>
2011-02-12 21:23:52 -0500

    Implement Physics::Space#remove.

--- Physics/Space.rb
+++ Physics/Space.rb
@@ -28,8 +28,26 @@ module Physics
 			end
 		end
 		
-		def remove
+		def remove(physics_obj)
+			#Add body to space
+			@space.remove_body physics_obj.bottom.body
+			@space.remove_body physics_obj.side.body
 			
+			#Add shape to space.  This depends on whether or not the shape is static.
+			if physics_obj.is_a? NonstaticObject
+				#Object is nonstatic
+				@space.remove_shape physics_obj.bottom
+				@space.remove_shape physics_obj.side
+			else
+				#Object is static
+				
+				#Static objects also have a render object which must be added
+				@space.remove_body physics_obj.render_object.body
+				
+				@space.remove_static_shape physics_obj.bottom
+				@space.remove_static_shape physics_obj.side
+				@space.remove_static_shape physics_obj.render_object
+			end
 		end
 		
 		def find
