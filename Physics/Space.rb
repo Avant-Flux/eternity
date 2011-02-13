baf42409130fa61a092953e95f22f67db2916eba
Raven <RavensKrag@gmail.com>
2011-02-12 20:40:04 -0500

    Create skeleton of new physics space.

---
+++ Physics/Space.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby
+ 
+module Physics
+	class Space
+		def initialize
+			@space = CP::Space.new
+		end
+		
+		def add
+			
+		end
+		
+		def remove
+			
+		end
+		
+		def find
+			
+		end
+	end
+end
