e04331de404112829deb58291aefc9068eef7b71
Raven <RavensKrag@gmail.com>
2011-02-13 04:21:50 -0500

    Comment out stuff to minimize breakage.

--- init_game.rb
+++ init_game.rb
@@ -45,35 +45,33 @@ class Game_Window < Gosu::Window
 			new_sequence :super2, [Gosu::KbLeftShift, Gosu::KbP]
 			new_combo :super3, [Gosu::KbQ, Gosu::KbJ, Gosu::KbK], [1000, 500, 200]
 		end
-		
-		$space = init_CP_Space3D
-		
+				
 		Building.new(:dimensions => [5, 6.5, 2], :position => [6, 11, 0])
 		Building.new(:dimensions => [3, 3, 1], :position => [8, 14, 0])
 		
-		Building.new(:dimensions => [5, 6.5, 2], :position => [15, 11, 0])
-		Building.new(:dimensions => [5, 6.5, 4], :position => [20, 11, 0])
-		Building.new(:dimensions => [5, 3, 2], :position => [20, 14, 0])
-		Building.new(:dimensions => [5, 6.5, 2], :position => [25, 11, 0])
-		Building.new(:dimensions => [5, 6.5, 2], :position => [20, 11-6.5, 0])
-		
-		Building.new(:dimensions => [5, 6.5, 2], :position => [15-50, 11, 0])
-		Building.new(:dimensions => [5, 6.5, 4], :position => [20-50, 11, 0])
-		Building.new(:dimensions => [5, 3, 2], :position => [20-50, 14, 0])
-		Building.new(:dimensions => [5, 6.5, 2], :position => [25-50, 11, 0])
-		Building.new(:dimensions => [5, 6.5, 2], :position => [20-50, 11-6.5, 0])
-		
-		Building.new(:dimensions => [5, 6.5, 2], :position => [15, 11-50, 0])
-		Building.new(:dimensions => [5, 6.5, 4], :position => [20, 11-50, 0])
-		Building.new(:dimensions => [5, 3, 2], :position => [20, 14, 0])
-		Building.new(:dimensions => [5, 6.5, 2], :position => [25, 11-50, 0])
-		Building.new(:dimensions => [5, 6.5, 2], :position => [20, 11-6.5-50, 0])
-		
-		Building.new(:dimensions => [5, 6.5, 2], :position => [15, 11+50, 0])
-		Building.new(:dimensions => [5, 6.5, 4], :position => [20, 11+50, 0])
-		Building.new(:dimensions => [5, 3, 2], :position => [20, 14+50, 0])
-		Building.new(:dimensions => [5, 6.5, 2], :position => [25, 11+50, 0])
-		Building.new(:dimensions => [5, 6.5, 2], :position => [20, 11-6.5+50, 0])
+		#~ Building.new(:dimensions => [5, 6.5, 2], :position => [15, 11, 0])
+		#~ Building.new(:dimensions => [5, 6.5, 4], :position => [20, 11, 0])
+		#~ Building.new(:dimensions => [5, 3, 2], :position => [20, 14, 0])
+		#~ Building.new(:dimensions => [5, 6.5, 2], :position => [25, 11, 0])
+		#~ Building.new(:dimensions => [5, 6.5, 2], :position => [20, 11-6.5, 0])
+		#~ 
+		#~ Building.new(:dimensions => [5, 6.5, 2], :position => [15-50, 11, 0])
+		#~ Building.new(:dimensions => [5, 6.5, 4], :position => [20-50, 11, 0])
+		#~ Building.new(:dimensions => [5, 3, 2], :position => [20-50, 14, 0])
+		#~ Building.new(:dimensions => [5, 6.5, 2], :position => [25-50, 11, 0])
+		#~ Building.new(:dimensions => [5, 6.5, 2], :position => [20-50, 11-6.5, 0])
+		#~ 
+		#~ Building.new(:dimensions => [5, 6.5, 2], :position => [15, 11-50, 0])
+		#~ Building.new(:dimensions => [5, 6.5, 4], :position => [20, 11-50, 0])
+		#~ Building.new(:dimensions => [5, 3, 2], :position => [20, 14, 0])
+		#~ Building.new(:dimensions => [5, 6.5, 2], :position => [25, 11-50, 0])
+		#~ Building.new(:dimensions => [5, 6.5, 2], :position => [20, 11-6.5-50, 0])
+		#~ 
+		#~ Building.new(:dimensions => [5, 6.5, 2], :position => [15, 11+50, 0])
+		#~ Building.new(:dimensions => [5, 6.5, 4], :position => [20, 11+50, 0])
+		#~ Building.new(:dimensions => [5, 3, 2], :position => [20, 14+50, 0])
+		#~ Building.new(:dimensions => [5, 6.5, 2], :position => [25, 11+50, 0])
+		#~ Building.new(:dimensions => [5, 6.5, 2], :position => [20, 11-6.5+50, 0])
 		
 		@player = Player.new("Raven", [5, 5, 0])
 		@characters = Array.new
@@ -83,17 +81,17 @@ class Game_Window < Gosu::Window
 			
 			@characters << Character.new("NPC", [x, y, 0])
 		end
-		@player.track(@characters[0])
-		@player.track(@characters[9])
-		@player.track(@characters[12])
-		@player.track(@characters[3])
-		@player.track(@characters[18])
+		#~ @player.track(@characters[0])
+		#~ @player.track(@characters[9])
+		#~ @player.track(@characters[12])
+		#~ @player.track(@characters[3])
+		#~ @player.track(@characters[18])
 		
 		@characters[0].say "hello world"
 		@characters[2].say "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend lacus quis dolor semper a faucibus nulla pharetra. Fusce venenatis posuere libero, aliquam malesuada lectus tempus nec. Donec vel dapibus magna. Quisque iaculis enim nec eros pharetra placerat. Sed enim metus, lobortis sed varius quis, interdum ac libero. Vivamus risus."
 		
 		@UI = UI::Overlay::Status.new(@player)
-		$camera = Camera.new(@player)
+		#~ $camera = Camera.new(@player)
 		
 		#@effect = Animations::Effect.new($window, "Gale")
 		#~ @background = Background.new($window,"Sprites/Textures/grass_texture2.png")
