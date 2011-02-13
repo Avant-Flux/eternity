8bb2ef4f88d8d9f6bd5be600113307b4afa30c5b
Raven <RavensKrag@gmail.com>
2011-02-12 20:50:31 -0500

    Refine Physics::Space#add to not use PhysicsObject#distinct_render?
    This method is unnecessary as only static objects will have a separate 
    render object.

--- Physics/Space.rb
+++ Physics/Space.rb
@@ -7,24 +7,24 @@ module Physics
 		end
 		
 		def add(physics_obj)
-			distinct = physics_obj.distinct_render?
-		
 			#Add body to space
 			@space.add_body physics_obj.bottom.body
 			@space.add_body physics_obj.side.body
-			@space.add_body physics_obj.render_object.body unless distinct
 			
 			#Add shape to space.  This depends on whether or not the shape is static.
 			if physics_obj.is_a? MovableObject
 				#Object is nonstatic
 				@space.add_shape physics_obj.bottom
 				@space.add_shape physics_obj.side
-				@space.add_shape physics_obj.render_object unless distinct
 			else
 				#Object is static
+				
+				#Static objects also have a render object which must be added
+				@space.add_body physics_obj.render_object.body
+				
 				@space.add_static_shape physics_obj.bottom
 				@space.add_static_shape physics_obj.side
-				@space.add_static_shape physics_obj.render_object unless distinct
+				@space.add_static_shape physics_obj.render_object
 			end
 		end
 		
