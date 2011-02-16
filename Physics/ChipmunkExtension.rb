2ef46f9205d12bdbc50f2c06c3073575ba9570b1
Raven <RavensKrag@gmail.com>
2011-02-16 07:43:26 -0500

    Have Chipmunk shapes and bodies refrence the physics object.

--- Physics/ChipmunkExtension.rb
+++ Physics/ChipmunkExtension.rb
@@ -6,21 +6,30 @@ require 'chipmunk-ffi'
 module Physics
 	module Shape
 		class Circle < CP::Shape::Circle
-			attr_reader :game_obj
+			attr_reader :physics_obj
 		
-			def initialize(game_obj, *args)
-				@game_obj = game_obj
+			def initialize(physics_obj, *args)
+				@physics_obj = physics_obj
 				super(*args)
 			end
 		end
 		
 		class Rect < CP::Shape::Rect
-			attr_reader :game_obj
+			attr_reader :physics_obj
 		
-			def initialize(game_obj, *args)
-				@game_obj = game_obj
+			def initialize(physics_obj, *args)
+				@physics_obj = physics_obj
 				super(*args)
 			end
+		end
+	end
+	
+	class Body < CP::Body
+		attr_reader :physics_obj
+	
+		def initialize(physics_obj, *args)
+			@physics_obj = physics_obj
+			super(*args)
 		end
 	end
 end
