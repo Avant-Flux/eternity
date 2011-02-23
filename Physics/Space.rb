a56dae08c56a9b59cb85c3544fc5f9780e326b96
Raven <RavensKrag@gmail.com>
2011-02-23 11:54:23 -0500

    Convert Physics::Space to a subclass of CP::Space.

--- Physics/Space.rb
+++ Physics/Space.rb
@@ -1,13 +1,16 @@
 #!/usr/bin/ruby
 
+require 'rubygems'
+require 'chipmunk-ffi'
+
 module Physics
-	class Space
+	class Space < CP::Space
 		#Create custom velocity and position functions for objects which respond to gravity. 
 		GRAVITY_VELOCITY_FUNC = Proc.new do |body, g, dmp, dt|
 			body.update_velocity(Physics::Space.g, Physics::Space.air_damping, dt)
-			
-			#If the player hits the ground
 			physics_obj = body.physics_obj
+			
+			#If the player hits the ground			
 			if physics_obj.pz < physics_obj.elevation
 				#Reset z-coordinate to be the same as the elevation
 				physics_obj.pz = physics_obj.elevation
@@ -32,10 +35,10 @@ module Physics
 		end
 		
 		def initialize(dt, g = -9.8, surface_damping=0.12, air_damping=1, iterations=10)
-			@space = CP::Space.new
-			@space.damping = surface_damping
-			@space.iterations = iterations
-			@space.gravity = CP::ZERO_VEC_2
+			super()
+			damping = surface_damping
+			iterations = iterations
+			gravity = CP::ZERO_VEC_2
 			
 			@dt = dt
 			@@g = CP::Vec2.new(0, g)
@@ -45,13 +48,13 @@ module Physics
 		end
 		
 		def step
-			@space.step @dt
+			super @dt
 		end
 		
 		def add(physics_obj)
 			#Add body to space
-			@space.add_body physics_obj.bottom.body
-			@space.add_body physics_obj.side.body
+			add_body physics_obj.bottom.body
+			add_body physics_obj.side.body
 			
 			#Add shape to space.  This depends on whether or not the shape is static.
 			if physics_obj.is_a? NonstaticObject
@@ -62,57 +65,45 @@ module Physics
 				physics_obj.bottom.body.velocity_func = COMPENSATION_VELOCITY_FUNC
 				
 				# Add shapes to space
-				@space.add_shape physics_obj.bottom
-				@space.add_shape physics_obj.side
+				add_shape physics_obj.bottom
+				add_shape physics_obj.side
 			else
 				# Add shapes to space
-				@space.add_static_shape physics_obj.bottom
-				@space.add_static_shape physics_obj.side
+				add_static_shape physics_obj.bottom
+				add_static_shape physics_obj.side
 			end
 		end
 		
 		def remove(physics_obj)
 			#Add body to space
-			@space.remove_body physics_obj.bottom.body
-			@space.remove_body physics_obj.side.body
+			remove_body physics_obj.bottom.body
+			remove_body physics_obj.side.body
 			
 			#Add shape to space.  This depends on whether or not the shape is static.
 			if physics_obj.is_a? NonstaticObject
 				#Object is nonstatic
-				@space.remove_shape physics_obj.bottom
-				@space.remove_shape physics_obj.side
+				remove_shape physics_obj.bottom
+				remove_shape physics_obj.side
 			else
 				#Object is static
 				
-				@space.remove_static_shape physics_obj.bottom
-				@space.remove_static_shape physics_obj.side
+				remove_static_shape physics_obj.bottom
+				remove_static_shape physics_obj.side
 			end
 		end
 		
 		def add_2D(obj)
-			@space.add_shape obj.shape
-			@space.add_body obj.shape.body
+			add_shape obj.shape
+			add_body obj.shape.body
 		end
 		
 		def remove_2D(obj)
-			@space.remove_shape obj.shape
-			@space.remove_body obj.shape.body
+			remove_shape obj.shape
+			remove_body obj.shape.body
 		end
 		
 		def find
 			
-		end
-		
-		def step
-			@space.step @dt
-		end
-		
-		def add_collision_handler(*args)
-			@space.add_collision_handler *args
-		end
-		
-		def add_collision_func(a,b,type=:pre,&block)
-			@space.add_collision_func a,b,type,&block
 		end
 		
 		class << self
