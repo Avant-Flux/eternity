4e2a99036b00a9bb845a79d4c63845d213bce2b1
Raven <RavensKrag@gmail.com>
2011-03-14 13:07:36 -0400

    Adjust when methods are fired based on proximity to ground.
    Must remember to take rounding errors into account here.  As doubles are
    being used in the backend, it is impossible to calculate exact equivalence.

--- Physics/SpaceVelocityFunctions.rb
+++ Physics/SpaceVelocityFunctions.rb
@@ -5,15 +5,17 @@ module Physics
 		#Create custom velocity and position functions for objects which respond to gravity. 
 		GRAVITY_VELOCITY_FUNC = Proc.new do |body, g, dmp, dt|
 			physics_obj = body.physics_obj
+			rise = physics_obj.pz - physics_obj.elevation
 			
-			if physics_obj.pz > physics_obj.elevation
-				#~ physics_obj.set_elevation
-				body.update_velocity($space.g, $space.air_damping, dt)
-				
-			else
+			upper_bound = 0.0015
+			lower_bound = -0.01
+			
+			if rise < lower_bound
+				puts 1
 				#Reset z-coordinate to be the same as the elevation
 				#When setting position, always set velocity as well.
 				physics_obj.reset_forces
+				physics_obj.side.body.reset_forces
 				physics_obj.vz = 0
 				physics_obj.raise_to_elevation
 				
@@ -21,9 +23,36 @@ module Physics
 				#Do things that need to be done when hitting the ground.
 				physics_obj.entity.resolve_ground_collision
 				physics_obj.entity.resolve_fall_damage body.v.y
-				#~ 
+			elsif rise > upper_bound
+				#~ puts 2
+				body.update_velocity($space.g, $space.air_damping, dt)
+			elsif rise > lower_bound && rise < upper_bound
+				#~ puts 3
 				body.update_velocity(CP::ZERO_VEC_2, $space.air_damping, dt)
 			end
+			
+			
+			#~ case physics_obj.pz - physics_obj.elevation <=> 0.15
+				#~ when -1
+					#~ puts -1
+					#~ #Reset z-coordinate to be the same as the elevation
+					#~ #When setting position, always set velocity as well.
+					#~ physics_obj.reset_forces
+					#~ physics_obj.side.body.reset_forces
+					#~ physics_obj.vz = 0
+					#~ physics_obj.raise_to_elevation
+					#~ 
+					#~ 
+					#~ #Do things that need to be done when hitting the ground.
+					#~ physics_obj.entity.resolve_ground_collision
+					#~ physics_obj.entity.resolve_fall_damage body.v.y
+				#~ when 0
+					#~ puts 0
+					#~ body.update_velocity(CP::ZERO_VEC_2, $space.air_damping, dt)
+				#~ when 1
+					#~ puts 1
+					#~ body.update_velocity($space.g, $space.air_damping, dt)
+			#~ end
 		end
 		
 		# Apply this function to the bottom object to get the side to move to compensate
@@ -32,9 +61,11 @@ module Physics
 			body.update_velocity(CP::ZERO_VEC_2, dmp, dt)
 			
 			#BUG does not allow jumping up-left.
-			if body.rot.y != 0
-				body.physics_obj.side.body.p.y += body.v.y*dt
-			end
+			#~ if body.v.y != 0
+				#~ body.physics_obj.side.body.v.y -= body.v.y + (body.f.y/body.m)*(dt)
+				#~ body.physics_obj.side.body.v.y += body.v.y
+				#~ body.physics_obj.side.body.p.x += body.v.x*dt + (body.f.x/body.m)*(dt**2)
+			#~ end
 		end
 	end
 end
