de0c2ce3f7d17e4aae80e6c36ac98b9aa6ced416
Raven <RavensKrag@gmail.com>
2011-04-09 00:53:37 -0400

    Separate velocity calculation and ground collision.

--- Physics/SpaceVelocityFunctions.rb
+++ Physics/SpaceVelocityFunctions.rb
@@ -7,47 +7,14 @@ module Physics
 			physics_obj = body.physics_obj
 			rise = physics_obj.pz - physics_obj.elevation
 			
-			upper_bound = 0.0015
-			lower_bound = -0.01
+			upper_bound = 0.0
+			lower_bound = 0.0
 			
-			if rise < lower_bound
-				#Hit ground
-				
-				#~ puts 1
-				#Reset z-coordinate to be the same as the elevation
-				#When setting position, always set velocity as well.
-				physics_obj.reset_forces
-				physics_obj.side.body.reset_forces
-				physics_obj.vz = 0
-				physics_obj.raise_to_elevation
-				
-				
-				#Do things that need to be done when hitting the ground.
-				physics_obj.entity.resolve_ground_collision
-				physics_obj.entity.resolve_fall_damage body.v.y
-			elsif rise > upper_bound
-				#In air
-			
-				#~ puts 2
+			if in_air
 				body.update_velocity($space.g, $space.air_damping, dt)
-				
-			elsif rise > lower_bound && rise < upper_bound
-				#On ground
-			
-				#~ puts 3
+			else #on_ground
 				body.update_velocity(CP::ZERO_VEC_2, $space.air_damping, dt)
-				if physics_obj.side.body.p.y > physics_obj.bottom.body.p.y
-					if body.v.y > 0
-						#If the position of the side is lower on the screen than of the bottom object
-						physics_obj.side.body.p.y = physics_obj.bottom.body.p.y
-						physics_obj.side.body.v.y = 0
-						physics_obj.bottom.body.reset_forces
-						physics_obj.bottom.body.v.y = 0
-					end
-				end
 			end
-			
-			
 		end
 		
 		# Apply this function to the bottom object to get the side to move to compensate
@@ -69,6 +36,42 @@ module Physics
 				#~ side_body.p.y += body.v.y*dt + (body.f.y/body.m)*(dt**2)
 				#~ side_body.v.y += body.v.y
 			#~ end
+		end
+	end
+	
+	module SpacePositionFunctions
+		GRAVITY_POSITION_FUNC = Proc.new do |body, dt|
+			#When setting position, always set velocity as well.
+		
+			#~ p_old = body.p #Will not work properly, as you are copying references
+			p_old_x = body.p.x
+			p_old_y = body.p.y
+			
+			body.update_position dt
+			
+			side_body = body.physics_obj.side.body
+			bottom_body = body.physics_obj.bottom.body
+			
+			# See if the object has collided with the ground.
+				# This position should not go "below" the y coordinate designated by the 
+				# bottom object.
+			if in_air
+				if side_body.p.y > bottom_body.p.y - elevation
+					# Set y equal to the level of the ground
+						#Reset z-coordinate to be the same as the elevation
+					body.physics_obj.raise_to_elevation
+						#~ side_body.p.y = bottom_body.p.y - elevation
+						
+					# Reset the velocity and force so the same thing does not happen on the next frame
+					side_body.v.y = 0
+					side_body.f.y = 0
+					
+					# At this point, a collisions with the ground has been detected and resolved
+					# Do things which need to be done on ground collision
+					physics_obj.entity.resolve_ground_collision
+					physics_obj.entity.resolve_fall_damage body.v.y
+				end
+			end
 		end
 	end
 end
