86b3a89f35b6c99511cb3d6e6dcffdc9c2986ca3
Raven <RavensKrag@gmail.com>
2011-04-30 20:43:52 -0400

    Update dependencies for gosu on linux.

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -2,8 +2,7 @@
 
 ruby_dev="ruby1.9.1-dev"
 ruby_opengl="libopengl-ruby1.9.1"
-gosu="g++ libgl1-mesa-dev libpango1.0-dev libboost-dev libaudiere-dev libsdl-ttf2.0-dev"
-rmagick="libmagickcore-dev libmagickwand-dev"
+gosu="g++ libgl1-mesa-dev libpango1.0-dev libboost-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libfreeimage3 libfreeimage-dev"
 
 install_command="gksudo apt-get install"
 
