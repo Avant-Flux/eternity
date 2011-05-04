a84d62b4e8388fd701c3d6bba296eaf6ebefec4b
Raven <RavensKrag@gmail.com>
2011-05-03 20:08:48 -0400

    Change ubuntu installer to wait for the dependencies to install.

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -4,10 +4,10 @@ ruby_dev="ruby1.9.1-dev"
 ruby_opengl="libopengl-ruby1.9.1"
 gosu="g++ libgl1-mesa-dev libpango1.0-dev libboost-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libfreeimage3 libfreeimage-dev"
 texplay="libglut3-dev"
-install_command="gksudo apt-get install"
+install_command="sudo apt-get install"
 
 dependency_installer="$install_command $ruby_dev $ruby_opengl $gosu $texplay"
 
-eval $dependency_installer
+eval $dependency_installer &&
 
 sudo gem install rake gosu chingu chipmunk texplay eventmachine require_all
