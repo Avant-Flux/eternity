95f06e9c729500254af090fc2d35fb8a5963a4f1
Raven <RavensKrag@gmail.com>
2011-05-28 02:51:50 -0400

    Update space velocity funciton.

--- Physics/SpaceVelocityFunctions.rb
+++ Physics/SpaceVelocityFunctions.rb
@@ -30,25 +30,33 @@ module Physics
 			body.update_position dt
 			
 			# See if the object has collided with the ground.
-				# This position should not go "below" the y coordinate designated by the 
+				# This position should not go "below" the y coordinate designated by the
 				# bottom object.
-			if rise < physics_obj.elevation
-				if side_body.p.y > bottom_body.p.y - body.physics_obj.elevation
-					# Set y equal to the level of the ground
-						#Reset z-coordinate to be the same as the elevation
-					#~ physics_obj.raise_to_elevation
-					side_body.p.y = bottom_body.p.y - physics_obj.elevation
-						
-					# Reset the velocity and force so the same thing does not happen on the next frame
-					side_body.v.y = 0
-					side_body.f.y = 0
-					bottom_body.v.y = 0
+			if rise < 0
+				# Set y equal to the level of the ground
+					#Reset z-coordinate to be the same as the elevation
 					
-					# At this point, a collisions with the ground has been detected and resolved
-					# Do things which need to be done on ground collision
-					physics_obj.entity.resolve_ground_collision
-					physics_obj.entity.resolve_fall_damage body.v.y
-				end
+				# Raise the object to the proper elevation
+				side_body.p.y = bottom_body.p.y - physics_obj.elevation
+					#~ physics_obj.raise_to_elevation
+				
+				# At this point, a collisions with the ground has been detected and resolved
+				# Do things which need to be done on ground collision
+				entity = body.physics_obj.entity
+				entity.resolve_ground_collision
+				entity.resolve_fall_damage body.v.y
+				
+				# Reset the velocity and force so the same thing does not happen on the next frame
+				
+				side_body.v.y = 0
+				side_body.f.y = 0
+				bottom_body.v.y = 0
+				bottom_body.f.y = 0
+				
+				side_body.v.x = 0
+				side_body.f.x = 0
+				bottom_body.v.x = 0
+				bottom_body.f.x = 0
 			end
 		end
 	end
