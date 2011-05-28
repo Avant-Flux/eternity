c4a71c5f719fad526f2679701d3a777391d4d530
Raven <RavensKrag@gmail.com>
2011-05-28 02:52:59 -0400

    Update ubuntu installer with dependencies for Bullet physics.

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -4,10 +4,12 @@ ruby_dev="ruby1.9.1-dev"
 ruby_opengl="libopengl-ruby1.9.1"
 gosu="g++ libgl1-mesa-dev libpango1.0-dev libboost-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libfreeimage3 libfreeimage-dev"
 texplay="libglut3-dev"
+nokogiri="libxslt-dev libxml2-dev"
+
 install_command="sudo apt-get install"
 
-dependency_installer="$install_command $ruby_dev $ruby_opengl $gosu $texplay"
+dependency_installer="$install_command $ruby_dev $ruby_opengl $gosu $texplay $nokogiri"
 
 eval $dependency_installer &&
 
-sudo gem install rake gosu chingu chipmunk texplay eventmachine require_all
+sudo gem install rake gosu chingu chipmunk texplay eventmachine require_all ffi-swig-generator
