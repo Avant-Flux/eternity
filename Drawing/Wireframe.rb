dada9026861d2f36888862b70b1ebee7b5dfcbb5
Raven <RavensKrag@gmail.com>
2011-06-16 13:11:50 -0400

    Overhaul wireframe and create new basic box wireframe class.

--- Drawing/Wireframe.rb
+++ Drawing/Wireframe.rb
@@ -22,10 +22,122 @@ module Gosu
 end
 
 module Wireframe
-	class Creature
+	# Contains functionally common to all wireframe classes
+	class WireframeObj
+		def initialize(window, entity)
+			@window = window
+			@entity = entity
+			
+			@visible = true
+		end
+		
+		def toggle_visibility
+			@visible = !@visible
+		end
+		
+		def visible?
+			@visible
+		end
 	end
 
-	class Character
+	# Draw a box in perspective
+	class Box < WireframeObj
+		def initialize(window, entity)
+			super(window, entity)
+			@window = window
+			
+			# Calculate the four points which comprise the bottom
+			# as well as the height of the box. This will allow for
+			# the construction of the box
+			
+			# Returns an array with references to all vertices
+			# which form the polygon base
+			vertices = entity.vertices
+			vertices << vertices[0]
+			
+			@vertices = []
+			vertices.each do |v|
+				@vertices << [v.p.x.to_px, v.p.y.to_px]
+			end
+			
+			@height = entity.height(:px)
+		end
+		
+		def update
+			
+		end
+		
+		def draw
+			@vertices.each_with_index do |vertex, i|
+				next_vertex = @vertices[i+1]
+				break unless next_vertex
+								
+				# Current vertex to next vertex
+				@window.draw_line	vertex[0], vertex[1], color,
+									next_vertex[0], next_vertex[1], color,
+									z, draw_mode
+				
+				# Point above current vertex to point above next vertex
+				@window.draw_line	vertex[0], vertex[1] - @height, color,
+									next_vertex[0], next_vertex[1] - @height, color,
+									z, draw_mode
+				
+				# Current vertex to point above current vertex
+				@window.draw_line	vertex[0], vertex[1], color,
+									vertex[0], vertex[1] - @height, color,
+									z, draw_mode
+			end
+		end
+	end
+	
+	# Similar to box, but with a sloped top surface
+	class Incline < WireframeObj
+		def initialize
+			# Define the height proc in such a way that (numeric - proc) 
+			# and other similar mathematics functions will be defined.
+		end
+		
+		def update
+			
+		end
+		
+		def draw
+			
+		end
+	end
+	
+	# Uses a non-perspective warped circle as a base
+	class Cylinder < WireframeObj
+		def initialize
+			
+		end
+		
+		def update
+			
+		end
+		
+		def draw
+			
+		end
+	end
+	
+	# Line Box, but the base can be any polygon
+	class Prism < WireframeObj
+		def initialize
+			
+		end
+		
+		def update
+			
+		end
+		
+		def draw
+			
+		end
+	end
+	
+	# The prism variant of Incline
+	class IrregularPrism < WireframeObj
 		def initialize
 			
 		end
