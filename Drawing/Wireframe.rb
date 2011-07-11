4da19a4d8c8d97ed515f06e3ce0bb7e982f20d56
Raven <RavensKrag@gmail.com>
2011-07-11 18:02:25 -0400

    Figure out how to draw quads on the different sides of an object for UVing.

--- Drawing/Wireframe.rb
+++ Drawing/Wireframe.rb
@@ -93,6 +93,37 @@ module Wireframe
 									z
 			end
 		end
+		
+		def uv_map
+			# Top
+			@window.draw_quad	@vertices[0][0], @vertices[0][1] - @height, @color,
+								@vertices[1][0], @vertices[1][1] - @height, @color,
+								@vertices[2][0], @vertices[2][1] - @height, @color,
+								@vertices[3][0], @vertices[3][1] - @height, @color,
+								1
+			
+			# Left
+				# Same as right, but shifted by X_Hat in the amount of width.
+				# OR can use the other two vertices
+			
+			
+			# Right
+			@window.draw_quad	@vertices[1][0], @vertices[1][1], @color,
+								@vertices[2][0], @vertices[2][1], @color,
+								@vertices[2][0], @vertices[2][1] - @height, @color,
+								@vertices[1][0], @vertices[1][1] - @height, @color,
+								1
+			
+			# Front
+			@window.draw_quad	@vertices[0][0], @vertices[0][1], @color,
+								@vertices[1][0], @vertices[1][1], @color,
+								@vertices[1][0], @vertices[1][1] - @height, @color,
+								@vertices[0][0], @vertices[0][1] - @height, @color,
+								1
+			# Back
+				# Same as front, but shifted by Y_HAT in the amount of depth
+				# OR could use the other two vertices
+		end
 	end
 	
 	# Similar to box, but with a sloped top surface
