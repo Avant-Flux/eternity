889eab24fb064ae3220674848685ed9d36a59dca
Raven <RavensKrag@gmail.com>
2011-08-15 19:42:35 -0400

    Create file for scrolling div widget.

---
+++ UI/Widgets/ScrollingDiv.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby
+require 'rubygems'
+require 'gosu'
+require 'texplay'
+require 'chipmunk'
+
+module Widget
+	# A roped-off zone where content can be rendered
+	# Other widgets, as well as Gosu::Image instances, should
+	# be able to exist within this context.
+	class ScrollingDiv < Div
+		def initialize(window, x, y, options={})
+			super window, x, y, options
+		end
+		
+		def update
+			super
+		end
+		
+		def draw
+			super
+		end
+	end
+end
