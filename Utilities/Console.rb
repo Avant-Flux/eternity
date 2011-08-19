8f3cad42a895093a97bf06936cf2753947894d25
Raven <RavensKrag@gmail.com>
2011-08-19 18:43:52 -0400

    Disable console.

--- Utilities/Console.rb
+++ Utilities/Console.rb
@@ -3,40 +3,40 @@
 #This class should reroute standard out and display it in a
 #text box on screen.  This way, errors etc can be displayed
 #in engine.
-class GosuConsole < TextBox
-	TOP_BUFFER = 20
-
-	def initialize(window, percent_of_height)
-		super(window, [0,TOP_BUFFER,0], window.width, ((window.height-TOP_BUFFER)*percent_of_height/100.0).round, nil)
-		@font = Gosu::Font.new(window, "Trebuchet MS", 20)
-		@visible = true
-	end
-	
-	def toggle_visibility
-		@visible = !@visible
-	end
-	
-	def visibility=(vis)
-		@visible = vis
-	end
-	
-	def visible?
-		@visible
-	end
-	
-	def pause
-		#Temporarily suspend output
-	end
-	
-	def stop
-		#Suspend output and clear output buffer
-	end
-	
-	def clear_buffer
-		
-	end
-	
-	def draw
-		super :z_offset => 1000
-	end
-end
+#~ class GosuConsole < TextBox
+	#~ TOP_BUFFER = 20
+#~ 
+	#~ def initialize(window, percent_of_height)
+		#~ super(window, [0,TOP_BUFFER,0], window.width, ((window.height-TOP_BUFFER)*percent_of_height/100.0).round, nil)
+		#~ @font = Gosu::Font.new(window, "Trebuchet MS", 20)
+		#~ @visible = true
+	#~ end
+	#~ 
+	#~ def toggle_visibility
+		#~ @visible = !@visible
+	#~ end
+	#~ 
+	#~ def visibility=(vis)
+		#~ @visible = vis
+	#~ end
+	#~ 
+	#~ def visible?
+		#~ @visible
+	#~ end
+	#~ 
+	#~ def pause
+		#~ #Temporarily suspend output
+	#~ end
+	#~ 
+	#~ def stop
+		#~ #Suspend output and clear output buffer
+	#~ end
+	#~ 
+	#~ def clear_buffer
+		#~ 
+	#~ end
+	#~ 
+	#~ def draw
+		#~ super :z_offset => 1000
+	#~ end
+#~ end
