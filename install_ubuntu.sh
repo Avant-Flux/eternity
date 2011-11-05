cbf07122ba0d3ff3f486b7668fa2e3c5481062a5
Raven <RavensKrag@gmail.com>
2011-11-05 14:16:00 -0400

    Make note about possibility of using freeglut3.

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -3,7 +3,7 @@
 ruby_dev="ruby1.9.1-dev"
 ruby_opengl="libopengl-ruby1.9.1"
 gosu="g++ libgl1-mesa-dev libpango1.0-dev libboost-dev libopenal-dev libsndfile-dev libxdamage-dev libsdl-ttf2.0-dev libfreeimage3 libfreeimage-dev"
-texplay="libglut3-dev"
+texplay="libglut3-dev" # or use freeglut3-dev, probably a better choice on Ubuntu
 rmagick="libmagickcore-dev libmagickwand-dev"
 
 install_command="sudo apt-get install"
