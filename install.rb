a733c80657d5530e3e6db1263fa70ed43a3d27a4
Raven <RavensKrag@gmail.com>
2012-06-03 22:37:11 -0400

    Only install bundler if not already installed.

--- install.rb
+++ install.rb
@@ -5,19 +5,22 @@ require 'rubygems'
 
 # Current installer will always attempt to install the bundler gem
 # TODO: Only install bundler if not already installed
-if RUBY_PLATFORM.downcase.include?("mswin32")
-	# Platform is Windows
-	puts "Windows!"
-	%x[gem install bundler]
-elsif RUBY_PLATFORM.downcase.include?("mingw")
-	# Not sure what this windows is, but it's what you get
-	# when you use the rubyinstaller.org version
-	puts "Still windows!"
-	%x[gem install bundler]
-else
-	# Assume all non-windows platforms are unix-based
-	puts "NOT windows!"
-	%x[sudo gem install bundler]
+gems = %x[gem list]
+unless gem.include? "bundler"
+	if RUBY_PLATFORM.downcase.include?("mswin32")
+		# Platform is Windows
+		puts "Windows!"
+		%x[gem install bundler]
+	elsif RUBY_PLATFORM.downcase.include?("mingw")
+		# Not sure what this windows is, but it's what you get
+		# when you use the rubyinstaller.org version
+		puts "Still windows!"
+		%x[gem install bundler]
+	else
+		# Assume all non-windows platforms are unix-based
+		puts "NOT windows!"
+		%x[sudo gem install bundler]
+	end
 end
 
 puts "Bundler ready"
