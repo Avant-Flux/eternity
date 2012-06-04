95c3585446735579fcedcf067f9ef6fc19c18d92
Raven <RavensKrag@gmail.com>
2012-06-03 21:16:38 -0400

    Output current platform to command line.

--- install.rb
+++ install.rb
@@ -3,14 +3,21 @@ Dir.chdir File.dirname(__FILE__)
 
 require 'rubygems'
 
+puts RUBY_PLATFORM.downcase
+sleep(30)
+
 begin
 	require 'bundler/setup'
 rescue LoadError
 	if RUBY_PLATFORM.downcase.include?("mswin32")
 		# Platform is Windows
+		puts "Windows!"
+		sleep(30)
 		%x[gem install bundler]
 	else
 		# Assume all non-windows platforms are unix-based
+		puts "NOT windows!"
+		sleep(30)
 		%x[sudo gem install bundler]
 	end
 	
