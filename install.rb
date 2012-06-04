13a86ef64c0d434064c473ff72863efdb06f4fe7
Raven <RavensKrag@gmail.com>
2012-06-03 21:33:29 -0400

    Installer should actually install dependencies before checking with bundler.

--- install.rb
+++ install.rb
@@ -3,25 +3,21 @@ Dir.chdir File.dirname(__FILE__)
 
 require 'rubygems'
 
-begin
-	require 'bundler/setup'
-rescue LoadError
-	if RUBY_PLATFORM.downcase.include?("mswin32")
-		# Platform is Windows
-		puts "Windows!"
-		%x[gem install bundler]
-	elsif RUBY_PLATFORM.downcase.include?("mingw")
-		# Not sure what this windows is, but it's what you get
-		# when you use the rubyinstaller.org version
-		puts "Still windows!"
-		%x[gem install bundler]
-	else
-		# Assume all non-windows platforms are unix-based
-		puts "NOT windows!"
-		%x[sudo gem install bundler]
-	end
-	
-	require 'bundler/setup'
+# Current installer will always attempt to install the bundler gem
+# TODO: Only install bundler if not already installed
+if RUBY_PLATFORM.downcase.include?("mswin32")
+	# Platform is Windows
+	puts "Windows!"
+	%x[gem install bundler]
+elsif RUBY_PLATFORM.downcase.include?("mingw")
+	# Not sure what this windows is, but it's what you get
+	# when you use the rubyinstaller.org version
+	puts "Still windows!"
+	%x[gem install bundler]
+else
+	# Assume all non-windows platforms are unix-based
+	puts "NOT windows!"
+	%x[sudo gem install bundler]
 end
 
 puts "Bundler ready"
