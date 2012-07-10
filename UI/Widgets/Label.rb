21f300abd86d77a1037bee43935a7a18701318ee
Raven <RavensKrag@gmail.com>
2012-07-10 15:16:33 -0400

    Make label decend from new Div class instead of UI_Object.

--- UI/Widgets/Label.rb
+++ UI/Widgets/Label.rb
@@ -5,106 +5,45 @@ require 'chipmunk'
 
 module Widget
 	# Similar to button, but not clickable
-	class Label < UI_Object
+	class Label < Div
 		include Widget::Background::Colored
 		
 		attr_reader :text
 		
 		def initialize(window, x, y, options={})
 			# The actual button event is processed within Chipmunk
-			options =	{
-							:z_index => 0,
-							:relative => window,
-							
-							:width => 1,
-							:width_units => :px,
-							:height => 1,
-							:height_units => :px,
-							
-							:background_color => Gosu::Color::NONE,
-							
-							:text_align => :center, #:center, :left, :right
-							:vertical_align => :middle, # :bottom, :middle, :top
-							
-							:font => nil,	# Font object used to render text
-							:text => nil,	# Text to be rendered on this label
-							:color => Gosu::Color::WHITE
-						}.merge! options
+			options = {
+				:text_align => :center, #:center, :left, :right
+				:vertical_align => :middle, # :bottom, :middle, :top
+				
+				:font => nil,	# Font object used to render text
+				:text => nil,	# Text to be rendered on this label
+				:color => Gosu::Color::WHITE
+			}.merge! options
 			
-			if options[:relative] != window
-				options[:z_index] += options[:relative].pz + 1
-			end
-			
-			super(window, options[:z_index])
-			
-			if options[:relative] != window
-				x += options[:relative].render_x
-				y += options[:relative].render_y
-			end
-			
-			@width =		case options[:width_units]
-							when :px
-								options[:width]
-							when :em
-								# Not defined for the window
-								options[:width] * options[:relative].font.text_width('m')
-							when :percent
-								# Specify :meters so that the measurement is not scaled
-								output = options[:relative].width
-								
-								
-								if options[:relative].respond_to? :padding
-									output -= options[:relative].padding[:left]
-									output -= options[:relative].padding[:right]
-								end
-								
-								(output * options[:width]/100.0).to_i
-						end
-					
-			@height =	case options[:height_units]
-							when :px
-								options[:height]
-							when :em
-								# Not defined for the window
-								options[:height] * options[:relative].font.text_width('m')
-							when :percent
-								# Specify :meters so that the measurement is not scaled
-								output =	if options[:relative].is_a? Gosu::Window
-										options[:relative].send :height
-									else
-										options[:relative].send :height, :meters
-									end
-									
-								if options[:relative].respond_to? :padding
-									output -= options[:relative].padding[:top]
-									output -= options[:relative].padding[:bottom]
-								end
-								
-								(output * options[:height]/100.0).to_i
-						end
-			
-			mass = 100
-			moment = 100
-			
-			#~ @body = CP::Body.new_static()
-			@body = CP::Body.new(mass, moment)
-			@shape = Physics::Shape::Rect.new self, @body, @width, @height
-			
-			@shape.collision_type = :button
-			@body.p = CP::Vec2.new(x,y)
-			
-			#~ init_physics [x,y], width, height, mass, moment, :button
+			super(window, x,y, options)
 			
 			if options[:text]
+				# TODO: Throw exception if other necessary parameters are not set.
 				@text = options[:text]
-				@font = options[:font]
-				@color = options[:color]
+				
+				
+				if options[:font]
+					@font = options[:font]
+				else
+					raise ArgumentError
+				end
+				
+				
+				if options[:color]
+					@color = options[:color]
+				else
+					raise ArgumentError
+				end
 				
 				text_align options[:text_align]
 				vertical_align options[:vertical_align]
 			end
-			
-			init_background options[:background_color]
 		end
 		
 		def update
@@ -137,9 +76,9 @@ module Widget
 				when :left
 					0
 				when :center
-					(@width - @font.text_width(@text))/2
+					(@shape.width - @font.text_width(@text))/2
 				when :right
-					(@width - @font.text_width(@text))
+					(@shape.width - @font.text_width(@text))
 			end
 		end
 		
@@ -150,11 +89,11 @@ module Widget
 				when :top
 					0
 				when :middle
-					(@height - @font.height)/2 + 2
+					(@shape.height - @font.height)/2 + 2
 					# Constant at the end may have to change
 					# with font or platform
 				when :bottom
-					@height - @font.height+5
+					@shape.height - @font.height+5
 					# Constant at the end may have to change
 					# with font or platform
 			end
