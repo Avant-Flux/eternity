b3eb48cd09bb0694ffc28f20384d5fb0946b5c46
Raven <RavensKrag@gmail.com>
2012-07-10 21:01:43 -0400

    Draw flux indicator image at the stored widget position.

--- UI/EternityWidgets/FluxIndicator.rb
+++ UI/EternityWidgets/FluxIndicator.rb
@@ -1,38 +1,16 @@
 class FluxIndicator < Widget::Div
 	def initialize(window, x,y, options={})
-		options = 	{
-			:z_index => 0,
-			:relative => window,
-			:align => :left,
-			
-			:position => :static, # static, dynamic, relative
-			
-			:width => 1,
-			:width_units => :px,
-			:height => 1,
-			:height_units => :px,
-			
-			:background_color => Gosu::Color::BLUE,
-			
-			:padding_top => 0,
-			:padding_bottom => 0,
-			:padding_left => 0,
-			:padding_right => 0,
-			
-			# Positioning
-			:top => 0,
-			:bottom => 0,
-			:left => 0,
-			:right => 0
-		}.merge! options
+		@flux_gear = Gosu::Image.new window,
+						"./Development/Interface/interface720/fluxgear.png", false
+		options[:width] = @flux_gear.width
+		options[:height] = @flux_gear.height
 		
 		super(window, x,y, options)
 		
 		@font = Gosu::Font.new @window, "Helvetica Bold", 25
 		
 		
-		@flux_gear = Gosu::Image.new window,
-						"./Development/Interface/interface720/fluxgear.png", false
+		
 	end
 	
 	def update
@@ -46,8 +24,9 @@ class FluxIndicator < Widget::Div
 		# White fill
 		scale_top_gear = 0.3
 		top_gear_offset_y = 150+20
-		@flux_gear.draw	(@window.width/2 - @flux_gear.width/2),
-							@window.height - top_gear_offset_y, 100
+		x = (@window.width/2 - @flux_gear.width/2)
+		y = @window.height - top_gear_offset_y
+		@flux_gear.draw	self.render_x, self.render_y, @pz
 	end
 end
 
