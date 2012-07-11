b528ce7b0d7f5f8a54d712a1db40efd417e03dca
Raven <RavensKrag@gmail.com>
2012-07-10 21:44:54 -0400

    Correctly style MP gauge with new system.

--- UI/EternityWidgets/MP_Indicator.rb
+++ UI/EternityWidgets/MP_Indicator.rb
@@ -1,61 +1,51 @@
 class MP_Indicator < Widget::Div
 	def initialize(window, x,y, options={})
-		options = 	{
-			:z_index => 0,
-			:relative => window,
-			:align => :left,
-			
-			:position => :static, # static, dynamic, relative
-			
-			:width => 1,
-			:width_units => :px,
-			:height => 1,
-			:height_units => :px,
-			
-			:background_color => Gosu::Color::BLUE,
-			
-			:padding_top => 0,
-			:padding_bottom => 0,
-			:padding_left => 0,
-			:padding_right => 0,
-			
-			# Positioning
-			:top => 0,
-			:bottom => 0,
-			:left => 0,
-			:right => 0
-		}.merge! options
-		
-		super(window, x,y, options)
-		@font = Gosu::Font.new @window, "Helvetica Bold", 25
+		@font = Gosu::Font.new window, "Helvetica Bold", 25
 				
 		# Background
-		#~ @mana_gear = Gosu::Image.new window, 
-					#~ "./Development/Interface/interface720/mpgear.png", false
-		
 		@mana_gear = Gosu::Image.new window,
 						"./Development/Interface/interface720/mpgear.png", false
+		
+		options[:height] = @mana_gear.height
+		options[:width] = @mana_gear.width
+		super(window, x,y, options)
 		
 		heath_label_offset_x = 80
 		width = 50
 		x = (@window.width/2 - width)-heath_label_offset_x
 		y = @window.height - 60 - 20
-		@mp_label = Widget::Label.new window, x,y,
-					:width => width, :height => @font.height,
-					#~ :background_color => Gosu::Color::GREEN,
-					:text => "MP", :font => @font, :color => Gosu::Color::RED,
-					:text_align => :center, :vertical_align => :top
+		@mp_label = Widget::Label.new window, 0,0,
+						:relative => self,
+						:width => width, :height => @font.height,
+						
+						:margin_right => 100,	:margin_right_units => :percent,
+						:margin_top => 10,		:margin_top_units => :px,
+						
+						:top => :auto,			:bottom => :auto,
+						:left => :auto,			:right => 0,
+						
+						:text => "MP", :font => @font, :color => Gosu::Color::RED,
+						:text_align => :center, :vertical_align => :top,
+						
+						:background_color => Gosu::Color::NONE
 		
 		heath_label_offset_x = 10
 		width = 100
 		x = (@window.width/2 - width) - heath_label_offset_x
 		y = @window.height - 35
-		z = 100
-		@mp_numerical_display = Widget::Label.new window, x,y,
-								:width => width, :height => @font.height,
-								#~ :background_color => Gosu::Color::FUCHSIA,
-								:text => "", :font => @font, :color => Gosu::Color::RED,
-								:text_align => :center, :vertical_align => :top
+		@mp_numerical_display = Widget::Label.new window, 0,0,
+									:relative => self,
+									:width => width, :height => @font.height,
+									
+									:margin_top => 100, :margin_top_units => :percent,
+									
+									:top => 0,			:bottom => :auto,
+									:left => :auto,		:right => -5,
+									
+									:text => "", :font => @font, :color => Gosu::Color::RED,
+									:text_align => :center, :vertical_align => :top,
+									
+									:background_color => Gosu::Color::FUCHSIA
 	end
 	
 	def update(mp, max_mp)
@@ -65,25 +55,11 @@ class MP_Indicator < Widget::Div
 	
 	def draw
 		super()
-		scale_side_gears = 0.25
-		#~ side_gear_offset_x = 55
-		side_gear_offset_x = 48
-		side_gear_offset_y = 45+40+20
-		
-		
 		# Mana
 		# Mana Orb
 		# Blue fill
-		x = (@window.width/2 - @mana_gear.width/2)-side_gear_offset_x
-		y = @window.height - side_gear_offset_y
-		z = 100
-		@mana_gear.draw x, y, z 
-		# Mana label
-		#~ heath_label_offset_x = 90
-		#~ x = (@window.width/2 - @mana_gear.width/2)-heath_label_offset_x
-		#~ y = @window.height - 60 - 20
-		#~ z = 100
-		#~ @font.old_draw "MP", x,y,z, 1,1, Gosu::Color::RED
+		@mana_gear.draw self.render_x, self.render_y, @pz 
+		
 		@mp_label.draw
 		# Mana level (text)
 		@mp_numerical_display.draw
