d12fd586bc4952ba3d3097b9058bf5b675652f75
Raven <RavensKrag@gmail.com>
2012-07-20 18:13:45 -0400

    Draw weapon durability indicator using gluPartialDisk instead of a mask.

--- UI/EternityWidgets/WeaponIndicator.rb
+++ UI/EternityWidgets/WeaponIndicator.rb
@@ -8,16 +8,9 @@ class WeaponIndicator < Widget::Div
 	# Establishes complete context for this widget, and maintains state of widget
 
 	def initialize(window, x,y, options={})
-		options =	{
-			:resolution => "720p"
-		}.merge! options
-		
 		# Caching which bypasses the asset-manager cache
-		@@weapon_gear ||= if options[:resolution] == "720p"
-			Gosu::Image.new window, "./Development/Interface/new_interface/left_weapon.png", false
-		elsif options[:resolution] == "1080p"
-			Gosu::Image.new window, "./Development/Interface/interface1080/weapongear.png", false
-		end
+		@@weapon_gear ||= Gosu::Image.new window, 
+							"./Development/Interface/new_interface/left_weapon.png", false
 		
 		options[:width] = @@weapon_gear.width
 		options[:height] = @@weapon_gear.height
@@ -30,6 +23,15 @@ class WeaponIndicator < Widget::Div
 	
 	def draw
 		super()
+		
+		color = Gosu::Color.rgba(0xf5a145ff)
+		#~ color = Gosu::Color::GREEN
+		percent = 75
+		@window.draw_circle	self.render_x+self.width/2, self.render_y+self.height/2, @pz,
+							self.width/2-12, color,
+							:stroke_width => 14, :alpha => 1.0,
+							:start_angle => 0, :angle => 360*percent/100.to_f
+		
 		@@weapon_gear.draw self.render_x, self.render_y, @pz
 		
 		
