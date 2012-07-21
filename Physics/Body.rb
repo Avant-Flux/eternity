07431f73a24e4f13457ac6d124a8af3f14444c90
Raven <RavensKrag@gmail.com>
2012-07-21 10:58:33 -0400

    Update elevation code, removing unnecessary calls.

--- Physics/Body.rb
+++ Physics/Body.rb
@@ -1,7 +1,7 @@
 module Physics
 	class Body < CP::Body
 		# TODO: Remove pointer to gameobject if it proves unnecessary
-		
+		# 		Currently only used for #resolve_ground_collision
 		attr_reader :gameobject
 		attr_accessor :pz, :vz, :az, :g
 		#~ attr_accessor :elevation
@@ -15,7 +15,6 @@ module Physics
 			
 			# TODO: Attempt to remove elevation if it is only needed for shadows
 			# Elevation is set by the space
-			#~ @elevation = nil
 			@elevation_heap = Containers::MaxHeap.new
 			@elevation_heap << 0
 			
@@ -51,12 +50,6 @@ module Physics
 		
 		def delete_elevation(elevation)
 			@elevation_heap.delete elevation
-			
-			if @elevation_heap.empty?
-				# This accounts for the player entering areas with no defined ground
-				puts "OUT OF BOUNDS"
-				@elevation_heap << 0
-			end
 		end
 		
 		def in_air?
