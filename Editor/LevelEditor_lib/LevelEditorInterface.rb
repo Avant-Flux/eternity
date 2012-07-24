90a4c4cb918f427fa6c1f8bf2e8dee9f1e29cd53
Raven <RavensKrag@gmail.com>
2012-07-24 19:23:46 -0400

    Create skeleton of tabed sidebar for level editor.

--- Editor/LevelEditor_lib/LevelEditorInterface.rb
+++ Editor/LevelEditor_lib/LevelEditorInterface.rb
@@ -13,8 +13,71 @@ class LevelEditorInterface
 		
 		#~ @mouse = MouseHandler.new space
 		@compass = Compass.new	window,
-								:top => :auto, :bottom => 0,
-								:left => 0, :right => :auto
+			:relative => window,
+			
+			:top => :auto, :bottom => 20,
+			:left => 20, :right => :auto
+		
+		@sidebar = Widget::Div.new	window,
+			:relative => window,
+			
+			:top => 0, :bottom => 0,
+			:left => :auto, :right => 0,
+			
+			:width => 300, :height => :auto,
+			
+			:background_color => Gosu::Color.rgba(255,255,255, 100)
+		@sidebar_title = Widget::Label.new window,
+			:relative => @sidebar,
+			
+			:top => 0, :bottom => :auto,
+			:left => :auto, :right => :auto,
+			
+			:width => 200, :height => @font.height,
+			
+			:font => @font, :text => "Object type", :color => Gosu::Color::WHITE,
+			
+			:background_color => Gosu::Color::BLACK
+		
+		tab_vert_offset = 30
+		@tabs = {
+			:static => Widget::Label.new(window,
+				:relative => @sidebar,
+				
+				:top => tab_vert_offset, :bottom => :auto,
+				:left => 0, :right => :auto,
+				
+				:width => 100, :height => @font.height,
+				
+				:font => @font, :text => "Static", :color => Gosu::Color::BLACK,
+				
+				:background_color => Gosu::Color::RED),
+			:entity => Widget::Label.new(window,
+				:relative => @sidebar,
+				
+				:top => tab_vert_offset, :bottom => :auto,
+				:left => :auto, :right => :auto,
+				
+				:width => 100, :height => @font.height,
+				
+				:font => @font, :text => "Entity", :color => Gosu::Color::BLACK,
+				
+				:background_color => Gosu::Color::GREEN
+			),
+			:spawn => Widget::Label.new(window,
+				:relative => @sidebar,
+				
+				:top => tab_vert_offset, :bottom => :auto,
+				:left => :auto, :right => 0,
+				
+				:width => 100, :height => @font.height,
+				
+				:font => @font, :text => "Spawn", :color => Gosu::Color::BLACK,
+				
+				
+				:background_color => Gosu::Color::BLUE
+			)
+		}
 	end
 	
 	def update
@@ -24,8 +87,10 @@ class LevelEditorInterface
 	def draw
 		@compass.draw
 		
-		#~ @sidebar.draw
-		#~ @sidebar_title.draw
+		@sidebar.draw
+		@sidebar_title.draw
+		@tabs.each_value {|tab| tab.draw}
+		
 		#~ @name_box.draw
 		#~ @load.draw
 		#~ @save.draw
