1d0455a723c81108cf5064df38239f3bacc9c486
Nathan Kwiatkowski <nathankski@gmail.com>
2012-08-20 19:31:03 -0400

    New class for mode selection

--- Editor/LevelEditor_lib/StateSelector.rb
+++ Editor/LevelEditor_lib/StateSelector.rb
@@ -1,30 +1,119 @@
 class StateSelector < LevelEditorInterface
-	def initialize(state_manager)
+	def initialize(window, space, font, state_manger)
+		super(window, space, font)
 		
+		init_widgets window
 		
-		@create = Widget::Button.new window,
-			:relative => @sidebar,
-			
-			:top => :auto, :bottom => 110,
-			:left => 10, :right => :auto,
-			
-			:width => 100, :height => @font.height,
-			:font => @font, :text => "Create", :color => Gosu::Color::BLACK do 
-				if @create_mode
-					@create_mode = false
-					@create.color = Gosu::Color::BLACK
-				else 
-					@create_mode = true
-					@create.color = Gosu::Color::WHITE
-				end
-			end
+		@button_width = 80
+		@active_mode = :default
+		
+		@state_manager = state_manager
 	end
 	
 	def update
-		
+		if @window.mouse_y <= (@topbar.body.p.y + @topbar.height)
+			@window.selected_cursor = :menu
+		end
 	end
 	
 	def draw
+		@topbar.draw
+		@topbar_title.draw
+		@create.draw
 		
+	end
+	
+	def init_widgets(window)
+	
+		@topbar = Widget::Div.new	window,
+			:relative => window,
+			
+			:top => 0, :bottom => :auto,
+			:left => 0, :right => :auto,
+			
+			:width => :auto, :height => @font.height+6,
+			
+			:background_color => Gosu::Color.rgba(255,255,255, 100)
+			
+		@topbar_title = Widget::Label.new window,
+			:relative => @sidebar,
+			
+			:top => 3, :bottom => :auto,
+			:left => 3, :right => :auto,
+			
+			:width => @button_width, :height => @font.height,
+			
+			:font => @font, :text => "Modes:", :color => Gosu::Color::WHITE,
+			
+			:background_color => Gosu::Color::BLACK
+			
+		@modes = {
+				
+			:create => Widget::Button.new(window,
+				:relative => @sidebar,
+				
+				:top => 3, :bottom => :auto,
+				:left => 86, :right => :auto,
+				
+				:width => @button_width, :height => @font.height,
+				:font => @font, :text => "Create", :color => Gosu::Color::BLACK)
+			do 
+				switch_to_mode :create
+			end
+			
+			:edit => Widget::Button.new(window,
+				:relative => @sidebar,
+				
+				:top => 3, :bottom => :auto,
+				:left => 166, :right => :auto,
+				
+				:width => @button_width, :height => @font.height,
+				:font => @font, :text => "Edit", :color => Gosu::Color::BLACK)
+			do 
+				switch_to_mode :edit
+			end
+			
+			:spawn => Widget::Button.new(window,
+				:relative => @sidebar,
+				
+				:top => 3, :bottom => :auto,
+				:left => 249, :right => :auto,
+				
+				:width => @button_width, :height => @font.height,
+				:font => @font, :text => "Spawn", :color => Gosu::Color::BLACK)
+			do 
+				switch_to_mode :spawn
+			end
+			
+			:default => Widget::Button.new(window,
+				:relative => @sidebar,
+				
+				:top => 3, :bottom => :auto,
+				:left => 332, :right => :auto,
+				
+				:width => @button_width, :height => @font.height,
+				:font => @font, :text => "Default", :color => Gosu::Color::WHITE)
+			do 
+				switch_to_mode :default
+			end
+		}
+	
+	end
+	
+	def click_create()
+		if @create_mode
+			@create_mode = false
+			@create.color = Gosu::Color::BLACK
+		else 
+			@create_mode = true
+			@create.color = Gosu::Color::WHITE
+		end
+	end
+	
+	def switch_to_mode(new_mode)
+		@modes[@active_mode].color = Gosu::Color::BLACK
+		@modes[new_mode].color = Gosu::Color::WHITE
+		
+		@active_mode = new_mode
 	end
 end
