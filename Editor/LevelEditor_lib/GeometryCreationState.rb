cf70e52ab348c6de33d72dcff6ea6588b6d6277a
Nathan Kwiatkowski <nathankski@gmail.com>
2012-08-20 22:09:05 -0400

    added labels for mouse input vars
    in geometrycreationstate

--- Editor/LevelEditor_lib/GeometryCreationState.rb
+++ Editor/LevelEditor_lib/GeometryCreationState.rb
@@ -1,6 +1,9 @@
 class GeometryCreationstate < LevelEditorInterface
-	def initialize
-
+	def initialize(window, space, font)
+		super(window, space, font)
+		init_widgets window
+		
+		@pos1 = CP::Vec2.new 0,0
 	end
 	
 	def update
@@ -8,17 +11,86 @@ class GeometryCreationstate < LevelEditorInterface
 	end
 	
 	def draw
-
-		
+		@sidebar.draw
+		@sidebar_title.draw
+		@point1.draw
+		@point2.draw
+		@position1.draw
+		@position2.draw
 	end
 
+	def init_widgets(window)
+		@sidebar = Widget::Div.new	window,
+			:relative => window,
+			
+			:top => 0, :bottom => 0,
+			:left => :auto, :right => 0,
+			
+			:width => 300, :height => :auto,
+			
+			:background_color => Gosu::Color.rgba(255,255,255, 100)
+		@sidebar_title = Widget::Label.new window,
+			:relative => @sidebar,
+			
+			:top => 0, :bottom => :auto,
+			:left => :auto, :right => :auto,
+			
+			:width => 200, :height => @font.height,
+			
+			:font => @font, :text => "Geometry Creation", :color => Gosu::Color::WHITE,
+			
+			:background_color => Gosu::Color::BLACK
+			
+		@point1 = Widget::Label.new window,
+			:relative => @sidebar,
+			
+			:top => 40, :bottom => :auto,
+			:left => 10, :right => :auto,
+			
+			:width => 80, :height => @font.height,
+			:font => @font, :text => "Point 1: ", :color => Gosu::Color::BLACK
+			
+		@point2 = Widget::Label.new window,
+			:relative => @sidebar,
+			
+			:top => 80, :bottom => :auto,
+			:left => 10, :right => :auto,
+			
+			:width => 80, :height => @font.height,
+			:font => @font, :text => "Point 2: ", :color => Gosu::Color::BLACK
+		
+		@position1 = Widget::Label.new window,
+			:relative => @sidebar,
+			
+			:top => 40, :bottom => :auto,
+			:left => 100, :right => :auto,
+			
+			:width => 80, :height => @font.height,
+			:font => @font, :text => "text", :color => Gosu::Color::BLACK
+		
+		@position2 = Widget::Label.new window,
+			:relative => @sidebar,
+			
+			:top => 80, :bottom => :auto,
+			:left => 100, :right => :auto,
+			
+			:width => 80, :height => @font.height,
+			:font => @font, :text => "text", :color => Gosu::Color::BLACK
+	
+	end
+	
 	
 	{
 		:add_widgets_to_space => :add_to_space,
 		:remove_widgets_from_space => :remove_from_space,
 	}.each do |interface_method, backend_method|
 		define_method interface_method do
-			#self.send backend_method, @stuff
+			self.send backend_method, @sidebar
+			self.send backend_method, @sidebar_title	
+			self.send backend_method, @point1
+			self.send backend_method, @point2
+			self.send backend_method, @position1
+			self.send backend_method, @position2
 		end
 	end
 
