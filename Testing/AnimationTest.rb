aa757d4bb42b19dbac4fe5d21b24dbe92723fa87
Raven <RavensKrag@gmail.com>
2012-08-31 23:07:16 -0400

    Create rudimentary basis for animation frame cache.

--- Testing/AnimationTest.rb
+++ Testing/AnimationTest.rb
@@ -10,6 +10,61 @@ require 'require_all'
 
 require 'gosu'
 
+
+class SpriteCache
+	# Caching has two levels:
+	# Assets not in VRAM will cache-miss and be fetched from RAM
+	# Assets not in RAM will cache-miss and be fetched from disk
+	# 
+	# Due to the speed of disk-fetching, assets should be kept in RAM for as long as possible.
+	# However, Gosu::Image.new loads into texture memory, so the caching is more of a recycle bin
+	# 
+	# LOAD ENTIRE SPRITESHEET INTO MEMORY AT ONCE
+	# Then, load frames into VRAM as necessary
+	def initialize(cache_size)
+		@size = cache_size
+		
+		# RAM Cache
+		# keys are animation identifiers, values are arrays of frames (raw RGBA blob data)
+		@cache = Hash.new
+		
+		# Frames currently in VRAM
+		# keys are full animation identifiers, values are arrays of Gosu::Image objects
+		@frames = Hash.new #
+	end
+	
+	# Returns one sprite for the specified frame of animation
+	def [](entity, action, direction, frame)
+		key = "#{entity}_#{action}_#{direction}"
+		
+		if @frames[key]
+			if @frames[key][frame]
+				return @frames[key][frame]
+			else
+				# Frame does not exist in VRAM.
+				# CACHE MISS
+				# Go to cache in RAM
+				
+			end
+		else
+			# Not a single frame from the animation requested is currently loaded into VRAM
+			
+		end
+	end
+	
+	private
+	
+	def load_sprite(entity, action, direction, frame)
+		filename = "/home/ravenskrag/Code/GameDev/Eternity/Renders/#{entity}/#{action}/#{direction}/Render1_"
+		filename << "%04i" % frame
+		filename << ".png"
+		
+		puts filename
+		
+		return Gosu::Image.new(self, filename, false)
+	end
+end
+
 #~ class Window < GameWindow
 class Window < Gosu::Window
 	def initialize
@@ -36,37 +91,33 @@ class Window < Gosu::Window
 		@tick = 0
 		@i = 1
 		spritesheet_filename =  "./Sprites/gearblade.dds"
-		#~ @frames = Gosu::Image::load_tiles(self, "./Sprites/Creatures/Golem_Walk_S.png", -8, -6, false)
-		#~ @frames2 = Gosu::Image::load_tiles(self, "./Sprites/Creatures/Golem_Walk_N.png", -8, -6, false)
 		
-		#~ @frames = []
-		#~ directions = ["South"].each do |direction|
-			#~ (1..48).each do |i|
-				#~ 
-				#~ 
-				#~ @frames << load_sprite("Golem", "Walk", direction, i)
-			#~ end
-		#~ end
+		# Raven:	Based on experimental data on my machine, the buffer should be about 60 sprites, 
+		# 			given the current size of the golem sprite.  However, the golem is the largest
+		# 			animated sprite thus far.  Ideally, the buffer size would be in bytes, not
+		# 			simply the number of frames.
+		# The buffer must load frames which have not yet been called, so that the game does not lag
+		# while trying to load new frames.  This may not actually be possible if the engine
+		# blocks on the IO operation of loading sprites from the disk.
 		
-		#~ @sprite = @frames[@i]
-		#~ @sprite = Gosu::Image.new self, "./Sprites/gearblade.dds", false
+		#~ @spritesheet = Gosu::Image.new(self, "/home/ravenskrag/Code/GameDev/Eternity/Renders/Golem/Walk/South/Render1_0031.png", false).to_blob
+		@spritesheet = ("\0"*4 * 900 * 900) # Uncompressed image data in RAM
 		
-		@sprite = Gosu::Image.new self, "/home/ravenskrag/Code/GameDev/Eternity/Renders/Golem/Walk/South/Render1_0031.png", false
-		#~ @sprite = load_sprite("Golem", "Walk", "South", @i)
+		@sprite = Gosu::Image.new(self, @spritesheet, false) # Load ONE sprite into VRAM
 	end
 	
 	def update
-		@tick += 1
-		if @tick == 2
-			@i += 1
+		#~ @tick += 1
+		#~ if @tick == 2
+			#~ @i += 1
 			#~ @i = 0 if @i == @frames.length
-			@i = 1 if @i == 49
-			
-			@tick = 0
-		end
+			#~ @i = 1 if @i == 49
+			#~ 
+			#~ @tick = 0
+		#~ end
 		
 		#~ @sprite = @frames[@i]
-		@sprite = load_sprite("Golem", "Walk", "North", @i)
+		#~ @sprite = load_sprite("Golem", "Walk", "North", @i)
 	end
 	
 	def draw
@@ -81,7 +132,7 @@ class Window < Gosu::Window
 		#~ puts position
 		x = self.width/2
 		y = self.height/4*3
-		@sprite.draw_rot x, y, 0, 0,0.5,1, @zoom, @zoom
+		#~ @sprite.draw_rot x, y, 0, 0,0.5,1, @zoom, @zoom
 		
 		format = "%0.3f"
 		@font.draw "zoom: #{format % @zoom}", 0,0,0
@@ -111,18 +162,6 @@ class Window < Gosu::Window
 	
 	def needs_cursor?
 		true
-	end
-	
-	private
-	
-	def load_sprite(entity, action, direction, frame)
-		filename = "/home/ravenskrag/Code/GameDev/Eternity/Renders/#{entity}/#{action}/#{direction}/Render1_"
-		filename << "%04i" % frame
-		filename << ".png"
-		
-		puts filename
-		
-		return Gosu::Image.new(self, filename, false)
 	end
 end
 
