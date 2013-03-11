3d70b04e517ec246e6d6da1df81a55b49405676a
Raven <RavensKrag@gmail.com>
2013-03-10 20:16:11 -0400

    Don't make LevelStateManager a decendent of StateManager.

--- GameStates/LevelStateManager.rb
+++ GameStates/LevelStateManager.rb
@@ -1,10 +1,8 @@
 # Manages the segments of the level
 # A level is made out one or more LevelState objects
 
-class LevelStateManager < StateManager
-	def initialize(window, space, layers, camera)
-		super(window, space, layers)
-		
+class LevelStateManager
+	def initialize
 		@camera = camera	# Camera used to cull which objects should be displayed
 		@current = 0		# Index into the "stack" which indicates which level the player is on
 	end
