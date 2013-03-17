0775d340f3d55b29e84adf288849e028a0d6ccfc
Raven <RavensKrag@gmail.com>
2013-03-17 15:47:47 -0400

    Create means of assigning unique ids to Model objects.
    No way to notify system that IDs can be reused yet though.

--- GameObjects/Components/Model.rb
+++ GameObjects/Components/Model.rb
@@ -1,54 +1,32 @@
 module Component
-	class Model
+	class Model < Oni::Model
+		MAX_NUMBER_OF_IDENTICAL_MESHES = 10000
+		
 		def initialize(window, mesh_name)
 			# TODO: Create better Model ID assigning algorithm
 			
-			@model = Oni::Agent.new window,	"#{mesh_name}_#{id_number mesh_name}", 
-											"#{mesh_name}.mesh"
-		end
-		
-		def update(dt)
-			@model.update dt # TODO: Move to animation component
-		end
-		
-		def visible?
-			@model.visible?
-		end
-		
-		def visible=(visibility)
-			@model.visible = visibility
+			super window, "#{mesh_name}_#{id_number mesh_name}", "#{mesh_name}.mesh"
 		end
 		
 		private
 		
 		def id_number(mesh_name)
-			# @@resources = {
-				# "name" => [count, stack]
-			# }
+			# Similar to cache mapping problem
+			# random assignment is good enough in most cases
+			# 
+			# randomly assign id numbers
+			# store assigned numbers in a set
+			# re-roll on collision
+			# remove ids as Mesh instances expire
+			# 	should this be done in a finalizer?
+			# 	should this move into the engine code so it can be explicitly triggered on GC?
+			@@ids ||= Hash.new
+			@@ids[mesh_name] ||= Set.new
 			
-			
-			@@resources ||= Hash.new # Store counts for each mesh, so each new mesh is given a new ID
-			
-			@@resources[mesh_name] ||= [0]
-			
-			count = @@resources[mesh_name].pop
-			@@resources[mesh_name] << count + 1
-			
-			
-			
-			
-			
-			
-			
-			if @@resources[mesh_name].size == 1
-				# Use the only number in the stack
-				count = @@resources[mesh_name].top
-			else
-				# Recycle old numbers before making new ones
-				
-			end
-			
-			@@resources[mesh_name] += 1
+			begin
+				@id = rand MAX_NUMBER_OF_IDENTICAL_MESHES
+				success = @@ids[mesh_name].add? @id
+			end until success
 		end
 	end
 end
