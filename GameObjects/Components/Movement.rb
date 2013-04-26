622dde444a35d9d9516ea65ed19a4ad6cd7f83b2
Raven <RavensKrag@gmail.com>
2013-04-26 14:52:09 -0400

    Snap angle of body if close enough to prevent jitter.

--- GameObjects/Components/Movement.rb
+++ GameObjects/Components/Movement.rb
@@ -257,6 +257,13 @@ module Component
 			
 			# Orient body relative to velocity
 			@physics.body.a = @physics.body.v.to_angle
+			# Snap to heading angle if close enough to remove stutter
+			heading_angle = @heading.to_angle
+			angle_error_bound = 2*Math::PI * 1/1000 # in radians
+			if @physics.body.a < heading_angle + angle_error_bound and
+			   @physics.body.a > heading_angle - angle_error_bound
+				@physics.body.a = heading_angle
+			end
 			
 			
 			# Forces applied relative to direction of the body
